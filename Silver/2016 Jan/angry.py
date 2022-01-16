d86cf44097ecb0f206a494feaba97c43d7316f5f
chenant2017 <chenant2017@gmail.com>
2022-01-16 15:58:21 -0800

    Update angry.py
    Used a pointer to eliminate need for set and pos

--- Silver/2016 Jan/angry.py
+++ Silver/2016 Jan/angry.py
@@ -8,35 +8,31 @@ def bin_search_R(min, max, K, hays):
   return max
 
 def check(K, R, hays):
-  pos = min(hays)
+  pointer = 0
   cows_left = K
-  while pos <= max(hays):
-    if pos in hays:
-      pos += 2 * R + 1
-      cows_left -= 1
-    if pos not in hays:
-      pos = find_next(pos, hays)
-      if pos == -1:
-        break
+
+  while hays[pointer] <= hays[-1]:
+    next_pos = hays[pointer] + 2 * R + 1
+    cows_left -= 1
+    while pointer < len(hays) and hays[pointer] < next_pos:
+      pointer += 1
+    if pointer == len(hays):
+      break
+    
   if cows_left < 0:
     return False
   return True
 
-def find_next(pos, hays):
-  for i in sorted(hays):
-    if i > pos:
-      return i
-  return -1
-
 def Run(input, output):
   with open(input, 'r') as fin, open(output, "w") as fout:
     N, K = (int(i) for i in fin.readline().split())
-    hays = set()
+    hays = []
     for i in range(N):
       hay = int(fin.readline())
-      hays.add(hay)
-    min_R = bin_search_R(0, max(hays)//2 + 1, K, hays)
+      hays.append(hay)
+    hays = sorted(hays)
+    min_R = bin_search_R(0, hays[-1]//2 + 1, K, hays)
     fout.write("{}\n".format(min_R))
-    #print(min_R)
+    print(min_R)
       
 Run("angry.in", "angry.out")
