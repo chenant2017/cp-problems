12550621220c17a473f0beda0179ae58d015f85f
chenant2017 <chenant2017@gmail.com>
2022-01-17 16:52:20 -0800

    Update socdist.py
    Off by 1

--- Silver/2020 Open/socdist.py
+++ Silver/2020 Open/socdist.py
@@ -1,38 +1,38 @@
-def test_space(space, grass, N):
-  cows_placed = 1
+def test_D(D, grass, N):
+  print("D", D)
   pos = grass[0][0]
+  length = grass[-1][-1]
+  cows_placed = 0
   interval = 0
-  while pos <= grass[-1][-1] and cows_placed < N: 
-    if in_interval(pos + space, grass[interval]):
-      cows_placed += 1
-      pos += space
-    else:
-      for i in range(interval + 1, len(grass)):
-        if pos + space < grass[i][0]:
-          pos = grass[i][0]
-          cows_placed += 1
-          interval = i
-          break
-        elif in_interval(pos + space, grass[i]):
-          pos += space
-          cows_placed += 1
-          interval = i
-          break
-        elif pos + space > grass[-1][-1]:
-          return False
-  return False if cows_placed < N else True
 
-def in_interval(pos, interval):
-  return True if (pos >= interval[0] and pos <= interval[1]) else False
+  while pos <= length:
+    print("pos", pos)
+    distance = grass[interval][1] - pos
+    cows_placed += distance // D + 1
+    pos += D * cows_placed
+
+    while interval < len(grass) and grass[interval][1] < pos:
+      interval += 1
+    print("interfva pos", interval, pos)
+    if interval == len(grass):
+      break
+    if pos < grass[interval][0]:
+      pos = grass[interval][0]
+  print("cows placed", cows_placed)
+  if cows_placed < N:
+    return False
+  return True
+      
+
+
 
 
 def bin_search(min_space, max_space, grass, N):
   best = min_space
-  jump = (min_space + max_space) // 2
+  jump = (best + max_space) // 2
   while jump > 0:
-    if test_space(min_space + jump, grass, N):
-      min_space += jump
-      best = min_space
+    if test_D(best + jump, grass, N):
+      best += jump
     else:
       jump //= 2 
   return best
@@ -41,15 +41,17 @@ def bin_search(min_space, max_space, grass, N):
 
 def Run(input, output):
   with open(input, 'r') as fin, open(output, "w") as fout:
-    N, M = (int(i) for i in fin.readline().split()) #of cows
+    N, M = (int(i) for i in fin.readline().split()) # N = cows, M = intervals
     grass = []
     for line in fin.readlines():
       start, end = (int(i) for i in line.split())
       grass.append((start, end))
     grass = sorted(grass)
+
     length = grass[-1][-1]
-    max_space = length // (N - 1)
+    max_space = length//N + 1
     best = bin_search(1, max_space, grass, N)
+    print(best)
     fout.write("{}\n".format(best))
 
     
