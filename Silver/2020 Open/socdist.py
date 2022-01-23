c25e32122cbf981239bbf2b523c9a7e2e41e8806
chenant2017 <chenant2017@gmail.com>
2022-01-22 16:31:09 -0800

    Update socdist.py
    Added binary search for interval but got time error for 4 cases; interval bin search failed more cases than w/o interval bin search

--- Silver/2020 Open/socdist.py
+++ Silver/2020 Open/socdist.py
@@ -1,44 +1,49 @@
 def test_D(D, grass, N):
-  print("D", D)
   pos = grass[0][0]
   length = grass[-1][-1]
   cows_placed = 0
   interval = 0
 
   while pos <= length:
-    print("pos", pos)
     distance = grass[interval][1] - pos
-    cows_placed += distance // D + 1
-    pos += D * cows_placed
+    new_cows = distance // D + 1
+    cows_placed += new_cows
+    if cows_placed >= N:
+      return True
+    pos += D * new_cows
 
-    while interval < len(grass) and grass[interval][1] < pos:
-      interval += 1
-    print("interfva pos", interval, pos)
+    interval = bin_search_interval(interval, grass, pos)
+
     if interval == len(grass):
       break
+
     if pos < grass[interval][0]:
       pos = grass[interval][0]
-  print("cows placed", cows_placed)
+    
   if cows_placed < N:
     return False
   return True
-      
-
-
-
 
 def bin_search(min_space, max_space, grass, N):
   best = min_space
-  jump = (best + max_space) // 2
+  jump = (max_space - best)
   while jump > 0:
     if test_D(best + jump, grass, N):
       best += jump
     else:
       jump //= 2 
   return best
-  
 
-
+def bin_search_interval(min_inter, grass, pos):
+  max_inter = len(grass)
+  while max_inter - min_inter > 1:
+    mid_inter = (min_inter + max_inter) // 2
+    if grass[mid_inter][1] < pos:
+      min_inter = mid_inter
+    else:
+      max_inter = mid_inter
+  return max_inter
+   
 def Run(input, output):
   with open(input, 'r') as fin, open(output, "w") as fout:
     N, M = (int(i) for i in fin.readline().split()) # N = cows, M = intervals
@@ -53,8 +58,5 @@ def Run(input, output):
     best = bin_search(1, max_space, grass, N)
     print(best)
     fout.write("{}\n".format(best))
-
-    
-    
 
 Run("socdist.in", "socdist.out")
