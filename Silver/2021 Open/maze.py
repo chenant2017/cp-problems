acb172f92e94cdbd0fb795bf1690e4107f2c50b5
chenant2017 <chenant2017@gmail.com>
2022-02-20 09:38:58 -0800

    Update maze.py
    Missed cases 7 and 10 because of time limit

--- Silver/2021 Open/maze.py
+++ Silver/2021 Open/maze.py
@@ -4,7 +4,7 @@ import os
 from collections import defaultdict
 
 def Run(input, output):
-  with open(input, 'r') as fin, open(output, "w") as fout:
+  with input as fin, output as fout:
     N = int(fin.readline())
     maze = [[None for __ in range(N)] for _ in range (N)]
     for i in range(N):
@@ -14,19 +14,13 @@ def Run(input, output):
         if space == "BBB":
           start = (i, j)
       fin.read(1)
-    for m in maze:
-      print(m)
-    ans = dfs(maze, [["." for _ in range(3)] for __ in range(3)], set(), start)
-    print("asdlkfj", ans)
+    boards = set()
+    dfs(maze, [["." for _ in range(3)] for __ in range(3)], set(), start, boards)
+    fout.write("{}\n".format(len(boards)))
 
-def dfs(maze, board, visited, start):
-  result = 0
+def dfs(maze, board, visited, start, boards):
   i, j = start
   visited.add((i, j, encode(board)))
-  print(i, j)
-  for b in board:
-    print(b)
-  print("-" * 15)
 
   if maze[i][j] != "..." and maze[i][j] != "BBB":
     letter, k, l = maze[i][j]
@@ -35,17 +29,18 @@ def dfs(maze, board, visited, start):
     if board[k][l] == ".":
       board[k][l] = letter
       if won(board):
+        boards.add(encode(board))
         board[k][l] = "."
-        return result + 1
+        return 
       for a, b in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
         if (a, b, encode(board)) not in visited and maze[a][b] != "###":
-          result += dfs(maze, board, visited, (a, b))
+          dfs(maze, board, visited, (a, b), boards)
       board[k][l] = "."
-  else:
-    for a, b in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
-      if (a, b, encode(board)) not in visited and maze[a][b] != "###":
-        result += dfs(maze, board, visited, (a, b))
-  return result
+      return 
+  for a, b in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
+    if (a, b, encode(board)) not in visited and maze[a][b] != "###":
+      dfs(maze, board, visited, (a, b), boards)
+  return 
 
 def won(board):
   for i in range(3):
@@ -74,14 +69,4 @@ def encode(board):
       power -= 1
   return total
 
-def RunFileWrapper(input, output):
-  fin = os.open(input, os.O_RDONLY)
-  with open(output, 'w') as fout:
-    Run(fin, fout)
-  os.close(fin)
-
-def RunIO():
-  Run(0, sys.stdout)
-
-# RunIO()
-Run("fin", "fout")
+Run(sys.stdin, sys.stdout)
