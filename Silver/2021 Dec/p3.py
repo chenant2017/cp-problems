be492639a346499e237dda4a8ae4bd976684f2f8
chenant2017 <chenant2017@gmail.com>
2022-02-26 14:14:38 -0800

    Update p3.py
    Still 2/20 cases. Optimized by adding frequency dictionaries

--- Silver/2021 Dec/p3.py
+++ Silver/2021 Dec/p3.py
@@ -1,6 +1,7 @@
 import sys
 import io
 import os
+
 def Run(input, output):
   
   readline = io.BytesIO(
@@ -13,29 +14,38 @@ def Run(input, output):
   ## output using fout.write()
 
   N, M = (int(i) for i in readline().split())
-  intervals = []
+
+  a_ = {}
+  b_ = {}
   
   for n in range(N):
     a, b = (int(i) for i in readline().split())
-    intervals.append((a, b))
+    if a in a_:
+      a_[a] += 1
+    else:
+      a_[a] = 1
+    if b in b_:
+      b_[b] += 1
+    else:
+      b_[b] = 1
   
   starts = [0] * (2*M + 1)
   ends = [0] * (2*M + 1)
   
-  for i in range(len(intervals)):
-    for j in range(i, len(intervals)):
-      ai, bi = intervals[i]
-      aj, bj = intervals[j]
-      starts[ai + aj] += 1
-      ends[bi + bj] += 1
-      if i != j:
-        starts[ai + aj] += 1
-        ends[bi + bj] += 1
+  for i, ai in a_.items():
+    for j, aj in a_.items():
+      start = i + j
+      starts[start] += ai * aj
+  
+  for i, bi in b_.items():
+    for j, bj in b_.items():
+      end = i + j
+      ends[end] += bi * bj
   
   count = 0
   for i in range(2*M + 1):
     count += starts[i]
     output.write("{}\n".format(count))
     count -= ends[i]
-  
+
 Run(0, sys.stdout)
