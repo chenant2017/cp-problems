cdad982700d2087fdf8f5e1d0ccae33b62c6d083
chenant2017 <chenant2017@gmail.com>
2022-02-25 17:01:33 -0800

    Update p2.py
    Same number of cases passed (5/10); however, get_diff was changed to linear complexity

--- Silver/2021 Dec/p2.py
+++ Silver/2021 Dec/p2.py
@@ -1,10 +1,9 @@
 import sys
 import io
 import os
-from collections import defaultdict, deque
+from collections import deque
 
 def Run(input, output):
-  
   readline = io.BytesIO(
       os.read(
         input, 
@@ -18,38 +17,21 @@ def Run(input, output):
   
   for t in range(T):
     visited = set()
-    paths = defaultdict(list)
+    paths = {}
     N, M = (int(i) for i in readline().split()) # points, connections
-    min_cost = 2*N**2
 
     for m in range(M):
       f1, f2 = (int(i) for i in readline().split())
-      paths[f1].append(f2)
-      paths[f2].append(f1)
-
-    path1 = get_path(paths, 1, visited)
-    pathN = get_path(paths, N, visited)
-
-    diff = get_diff(path1, pathN)
-    cost = diff**2
-    if cost < min_cost:
-      min_cost = cost
-      if min_cost <= 1:
-        output.write("{}\n".format(min_cost))
-        continue
-
-    if len(visited) < N:
-      for i in range(2, N):
-        if i not in visited:
-          path = get_path(paths, i, visited)
-          diff1 = get_diff(path, path1)
-          diffN = get_diff(path, pathN)
-          cost = diff1**2 + diffN**2
-          if cost < min_cost:
-            min_cost = cost
-            if min_cost <= 2:
-              break
-
+      if f1 in paths:
+        paths[f1].append(f2)
+      else:
+        paths[f1] = [f2]
+      if f2 in paths:
+        paths[f2].append(f1)
+      else:
+        paths[f2] = [f1]
+    
+    min_cost = get_cost(paths, visited, N)
     output.write("{}\n".format(min_cost))
 
 def get_path(paths, start, visited):
@@ -59,20 +41,62 @@ def get_path(paths, start, visited):
     curr = queue.pop()
     path.append(curr)
     visited.add(curr)
-    for i in paths[curr]:
-      if i not in visited:
-        queue.append(i)
+    if curr in paths:
+      for i in paths[curr]:
+        if i not in visited:
+          queue.append(i)
   return sorted(path)
+
+def get_cost(paths, visited, N):
+  min_cost = 2*N**2
+  paths2 = []
+  path1 = None
+  pathN = None
+
+  for i in range(1, N + 1):
+    if i not in visited:
+      path = get_path(paths, i, visited)
+      if path1 is None and 1 in visited:
+        path1 = path
+        if pathN is None and N in visited:
+          return 0
+      elif pathN is None and N in visited:
+        pathN = path
+      else:
+        paths2.append(path)
+
+  diff = get_diff(path1, pathN)
+  cost = diff**2
+  if cost < min_cost:
+    min_cost = cost
+    if min_cost <= 1:
+      return min_cost
+
+  for p in paths2:
+    diff1 = get_diff(p, path1)
+    diffN = get_diff(p, pathN)
+    cost = diff1**2 + diffN**2
+    if cost < min_cost:
+      min_cost = cost
+      if min_cost <= 2:
+        return min_cost
+  
+  return min_cost
 
 def get_diff(path1, path2):
   min_diff = 2*10**5
-  for i in path1:
-    for j in path2:
-      if abs(i - j) < min_diff:
-        min_diff = abs(i - j)
-        if min_diff == 0:
-          return min_diff
+  i = 0
+  j = 0
+
+  while i < len(path1) and j < len(path2):
+    diff = abs(path2[j] - path1[i])
+    if diff < min_diff:
+      min_diff = diff
+    if path1[i] < path2[j]:
+      i += 1
+    else:
+      j += 1
+
   return min_diff
-
-
+    
 Run(0, sys.stdout)
