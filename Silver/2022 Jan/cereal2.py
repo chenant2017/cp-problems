74b2d1fdedd5b92f6f26104fbf61589a603673a1
chenant2017 <chenant2017@gmail.com>
2022-03-01 21:38:47 -0800

    Update cereal2.py
    No additional cases passed, but much shorter!

--- Silver/2022 Jan/cereal2.py
+++ Silver/2022 Jan/cereal2.py
@@ -14,94 +14,57 @@ def Run(input, output):
           adj[j].append(i)
           adj[i].append(j)
     
-    comps = get_comps(adj)
-    
     final_filled = 0
     final_path = []
-    for i in comps:
-      max_filled = -1
-      max_path = None
-      for j in i:
-        max_filled, max_path = dfs(cows, j, adj, max_filled, max_path, M)
-      final_filled += max_filled
-      final_path += max_path
+    visited = set()
+    for i in range(1, N + 1):
+      if i not in visited:
+        filled, path = dfs(cows, i, adj, M, visited)
+        final_filled += filled
+        final_path += path
 
-    rem = set(range(1, N + 1))
-    for i in final_path:
-      if i in rem:
-        rem.remove(i)
-    
-    for i in rem:
-      final_path.append(i)
-    
     fout.write("{}\n".format(N - final_filled))
     for i in final_path:
       fout.write("{}\n".format(i))
       
 
+
 from collections import deque
 
-def dfs(cows, start, adj, max_filled, max_path, M): #start is the cow int
+def dfs(cows, start, adj, M, visited): #start is the cow int
   N = len(cows) - 1
   path = []
   taken = [False] * (M + 1)
-  queue = deque([(start, path)])
-  visited = set()
+  queue = deque([start])
+  filled = 0
 
   while queue:
-    curr, path = queue.pop()
+    curr = queue.pop()
+    if curr in visited:
+      continue
     visited.add(curr)
+    path.append(curr)
     first, second = cows[curr]
 
     stop = False
 
     if taken[first]:
       if taken[second]:
-        filled = len(path)
-        if filled > max_filled:
-          max_filled = filled
-          max_path = path
         stop = True
       else:
-        path.append(curr)
         taken[second] = True
+        filled += 1
     else:
-      path.append(curr)
       taken[first] = True
+      filled += 1
     
     if not stop:
-      count = 0
       for i in adj[curr]:
         if i not in visited:
-          queue.append((i, path))
-          count += 1
-      if count == 0:
-        filled = len(path)
-        if filled > max_filled:
-          max_filled = filled
-          max_path = path
-  return max_filled, max_path
+          queue.append(i)
 
-def get_comps(adj):
-  visited = set()
-  comps = []
-  for i in adj:
-    if i not in visited:
-      comp = get_comp(i, adj, visited)
-      comps.append(comp)
-  return comps
+  return filled, path
 
-def get_comp(start, adj, visited):
-  comp = set()
-  queue = deque([start])
-  while queue:
-    curr = queue.popleft()
-    comp.add(curr)
-    visited.add(curr)
-    for i in adj[curr]:
-      if i not in comp:
-        queue.append(i)
-  return comp
   
 Run(sys.stdin, sys.stdout)
 
