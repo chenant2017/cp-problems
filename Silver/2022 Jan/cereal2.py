4cc74cd262c2c3745ab5bd26ea9d4f620a7bc5a6
chenant2017 <chenant2017@gmail.com>
2022-03-03 21:45:53 -0800

    Update cereal2.py
    Removed nested for loop when creating adjacency matrix
    
    Loop detection so that dfs only starts at a cow in a loop in the graph
    
    Changed dfs so that dfs is undirected.

--- Silver/2022 Jan/cereal2.py
+++ Silver/2022 Jan/cereal2.py
@@ -3,23 +3,27 @@ import sys
 def Run(input, output):
   with input as fin, output as fout:
     N, M = (int(i) for i in fin.readline().split()) #cows, cereals
-    adj = {}
+    cereals = {}
     cows = [None]
     for i in range(1, N + 1):
       a, b = (int(j) for j in fin.readline().split())
       cows.append((a, b))
-      adj[i] = []
-      for j in adj:
-        if cows[j][0] == a or cows[j][1] == a or cows[j][0] == b:
-          adj[j].append(i)
-          adj[i].append(j)
+      if a in cereals:
+        cereals[a].add(i)
+      else:
+        cereals[a] = {i}
+      if b in cereals:
+        cereals[b].add(i)
+      else:
+        cereals[b] = {i}
     
     final_filled = 0
     final_path = []
     visited = set()
     for i in range(1, N + 1):
       if i not in visited:
-        filled, path = dfs(cows, i, adj, M, visited)
+        start = get_start(cows, i, cereals, M)
+        filled, path = dfs(cows, start, cereals, M, visited)
         final_filled += filled
         final_path += path
 
@@ -27,11 +31,10 @@ def Run(input, output):
     for i in final_path:
       fout.write("{}\n".format(i))
       
-
-
+ 
 from collections import deque
 
-def dfs(cows, start, adj, M, visited): #start is the cow int
+def dfs(cows, start, cereals, M, visited): #start is the cow int
   N = len(cows) - 1
   path = []
   taken = [False] * (M + 1)
@@ -46,26 +49,48 @@ def dfs(cows, start, adj, M, visited): #start is the cow int
     path.append(curr)
     first, second = cows[curr]
 
-    stop = False
-
     if taken[first]:
-      if taken[second]:
-        stop = True
-      else:
+      if not taken[second]:
         taken[second] = True
         filled += 1
+        for i in cereals[second]:
+          if i not in visited:
+            queue.append(i)
+      else:
+        continue
     else:
       taken[first] = True
       filled += 1
-    
-    if not stop:
-      for i in adj[curr]:
+      for i in cereals[first]:
         if i not in visited:
           queue.append(i)
 
   return filled, path
 
-  
+def get_start(cows, start, cereals, M):
+  N = len(cows) - 1
+  queue = deque([start])
+  visited = set()
+  taken = [False] * (M + 1)
+
+  while queue:
+    curr = queue.pop()
+    if curr in visited:
+      continue
+    visited.add(curr)
+    first, second = cows[curr]
+
+    if taken[first] and taken[second]:
+      return curr
+    else:
+      taken[first] = True
+      taken[second] = True
+
+    for i in cereals[first] | cereals[second]:
+      if i not in visited:
+        queue.append(i)
+  return start
+
 Run(sys.stdin, sys.stdout)
 
 
