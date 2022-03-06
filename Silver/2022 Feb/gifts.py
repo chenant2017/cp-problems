c87eaed6b57602a63fb553777f7245dcf58d52eb
chenant2017 <chenant2017@gmail.com>
2022-03-06 15:11:28 -0800

    Update gifts.py
    Did not make for loop generalized

--- Silver/2022 Feb/gifts.py
+++ Silver/2022 Feb/gifts.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 def Run(fin, fout):
   N = int(fin.readline())
@@ -15,28 +16,28 @@ def Run(fin, fout):
       choice[j + 1] = lj
     choices[i] = choice
   #print("indices", indices)
-  for i in range(1, 5):
-    result = dfs(i, choices, indices, {}, {})
+  for i in range(1, N + 1):
+    result = dfs(i, choices, indices, defaultdict(int), defaultdict(int))
     fout.write("{}\n".format(result))
 
 def dfs(start, choices, indices, took, taken_by):
   N = len(choices) - 1
   ind = indices[start] #optimize by storing indices later
   #print(taken_by, took)
-  if start in took:
+  if took[start]:
     return took[start]
   for i in range(1, ind):
     j = choices[start][i]
-    if j not in taken_by:
+    if not taken_by[j]:
       taken_by[j] = start
       took[start] = j
       if dfs(j, choices, indices, took, taken_by) != j:
         #print("returned", j, taken_by)
         return j
-      else:
-        taken_by[j] = j
-        took[j] = j
+      else: #backtrack
+        taken_by[j] = 0
+        took[start] = 0
   #print("returned", start, taken_by)
   return start
-
+  
 Run(sys.stdin, sys.stdout)
