ffae054fe08c7c0e3edb9714b3a4d5afc04e243f
chenant2017 <chenant2017@gmail.com>
2022-03-06 10:26:39 -0800

    changed to 1d prefix sum w/ O(N^3)
    Pass 5/10: TLE 6-10.
    Maybe python issue.

--- Silver/2021 Feb/green.py
+++ Silver/2021 Feb/green.py
@@ -1,7 +1,7 @@
 import sys
 import io
 import os
-from collections import defaultdict
+import copy
 
 def Run(input, output):
   
@@ -11,78 +11,40 @@ def Run(input, output):
         os.fstat(input).st_size
       )
     ).readline
-  ## input using readline()
-  ## output using fout.write()
 
   N = int(readline())
-  green_100 = [[0 for _ in range(N)] for __ in range(N)]
-  green_u100 = [[0 for _ in range(N)] for __ in range(N)]
-
-  ans = 0
-  
+  grass = [None for _ in range(N)]
   for i in range(N):
-    row = [int(j) for j in readline().split()]
-    for k in range(N):
-      if row[k] == 100:
-        green_100[i][k] = 1
-        ans += 1
-      elif row[k] < 100:
-        green_u100[i][k] = 1
-  
-  make_prefix(green_100, N)
-  make_prefix(green_u100, N)
+    grass[i] = [int(i) for i in readline().split()]
+  prefix_100 = get_prefix(grass, N, 100)
+  prefix_101 = get_prefix(grass, N, 101)
+  rect_100 = get_n_rectangles(prefix_100, N)
+  rect_101 = get_n_rectangles(prefix_101, N)
+  output.write("{}\n".format(rect_100 - rect_101))
 
-  
-  for i1 in range(N):
-    for j1 in range(N):
-      min_bad_j = N
-      for i2 in range(i1, N):
-        for j2 in range(j1, N):
-          if i1 == i2 and j1 == j2:
-            continue
-          if j2 == min_bad_j:
-            break
-          if prefix_sum(green_100, i1, j1, i2, j2) >= 1:
-            if prefix_sum(green_u100, i1, j1, i2, j2) == 0:
-              ans += 1
-            else:
-              min_bad_j = j2
-              break
-
-
-
-  output.write("{}\n".format(ans))
-
-def prefix_sum(prefixes, min_i, min_j, max_i,max_j):
-  result = prefixes[max_i][max_j]
-  if min_i > 0:
-    result -= prefixes[min_i - 1][max_j]
-    if min_j > 0:
-      result += prefixes[min_i - 1][min_j - 1]
-  if min_j > 0:
-    result -= prefixes[max_i][min_j - 1]
-  
-  return result
-
-
-
-def make_prefix(matrix, N):
+def get_prefix(matrix, N, threshold):
+  prefix = copy.deepcopy(matrix)
   for i in range(N):
-    row = matrix[i]
-    for j in range(1, N):
-      row[j] += row[j - 1]
+    if prefix[i][-1] >= threshold:
+      prefix[i][-1] = 1
+    else:
+      prefix[i][-1] = 0
+    for j in range(N - 2, -1, -1):
+      if prefix[i][j] >= threshold:
+        prefix[i][j] = prefix[i][j + 1] + 1
+      else:
+        prefix[i][j] = 0
+  return prefix
 
-  for i in range(1, N):
+def get_n_rectangles(prefix, N):
+  total = 0
+  for i in range(N):
     for j in range(N):
-      matrix[i][j] += matrix[i - 1][j]
+      min = N
+      for k in range(i, N):
+        if prefix[k][j] < min:
+          min = prefix[k][j]
+        total += min
+  return total
 
-def RunFileWrapper(input, output):
-  fin = os.open(input, os.O_RDONLY)
-  with open(output, 'w') as fout:
-    Run(fin, fout)
-  os.close(fin)
-
-def RunIO():
-  Run(0, sys.stdout)
-
-RunIO()
+Run(0, sys.stdout)
