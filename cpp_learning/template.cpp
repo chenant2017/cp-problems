6d15d67bbdf3816c5339b6a12da9a62bcdb1a150
chenant2017 <chenant2017@gmail.com>
2022-06-05 20:25:08 -0700

    Update template.cpp
    name -> fname

--- cpp_learning/template.cpp
+++ cpp_learning/template.cpp
@@ -9,9 +9,9 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string name = "";
-	freopen((name + ".in").c_str(), "r", stdin);
-	freopen((name + ".out").c_str(), "w", stdout);
+	string fname = "";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	freopen((fname + ".out").c_str(), "w", stdout);
 	
 	return 0;
 }
