8b0400da88d6b893f0d9701ef9e8ef6b93c2552a
chenant2017 <chenant2017@gmail.com>
2022-06-23 21:52:30 -0700

    Create revegetate.cpp
    2/10

---
+++ cpp_learning/revegetate.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define MAX 100010
+#define f first
+#define s second
+
+using namespace std;
+
+ll N, M;
+
+vector<pair<ll, ll>> adj[MAX];
+ll colors[MAX] = {0};
+
+ll floodfill(ll start, ll color) {
+	if (colors[start] != 0) {
+		if (colors[start] == color) {
+			return 1;
+		}
+		return 0;
+	}
+
+	ll result = 0;
+	colors[start] = color;
+
+	for (auto& i: adj[start]) {
+		result = floodfill(i.f, color * i.s);
+	}
+
+	return result;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "revegetate";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N >> M;
+
+	for (ll i = 0; i < M; i++) {
+		ll a, b;
+		string sd;
+		cin >> sd >> a >> b;
+
+		if (sd[0] == 'S') {
+			adj[a].push_back({b, 1});
+			adj[b].push_back({a, 1});
+		}
+		else {
+			adj[a].push_back({b, -1});
+			adj[b].push_back({a, -1});
+		}
+	}
+
+	ll comps = 0;	
+
+	for (ll i = 1; i <= N; i++) {
+		if (colors[i] != 0) {
+			continue;
+		}
+		ll ff = floodfill(i, 1);
+		if (ff == 0) {
+			cout << "0\n";
+			return 0;
+		}
+		comps++;
+	}
+
+	cout << pow(10, comps) << "\n";
+
+	return 0;
+}
