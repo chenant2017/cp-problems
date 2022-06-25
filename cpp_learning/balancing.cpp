4695d6fdd1c8252eb5eb7d846d9bdd76ee977fdc
chenant2017 <chenant2017@gmail.com>
2022-06-24 20:21:40 -0700

    Create balancing.cpp
    1/10

---
+++ cpp_learning/balancing.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define MAXN 1000
+#define f first
+#define s second
+
+using namespace std;
+
+ll N;
+pair<ll, ll> cows[MAXN];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "balancing";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N;
+
+	for (ll i = 0; i < N; i++) {
+		cin >> cows[i].f >> cows[i].s;
+	}
+
+	sort(cows, cows + N);
+	vector<ll> possa;
+	if (N % 2 == 0) {
+		possa.insert(possa.end(), {cows[N/2 - 1].f + 1, cows[N/2 + 1].f - 1}); 
+	}
+	else {
+		possa.insert(possa.end(), {cows[N/2].f + 1, cows[N/2].f - 1}); 
+	}
+
+
+	sort(cows, cows + N, [](auto& a, auto& b) {
+		return a.s < b.s || (a.s == b.s && a.f < b.f);
+	});
+	vector<ll> possb;
+	if (N % 2 == 0) {
+		possb.insert(possb.end(), {cows[N/2 - 1].s + 1, cows[N/2 + 1].s - 1}); 
+	}
+	else {
+		possb.insert(possb.end(), {cows[N/2].s + 1, cows[N/2].s - 1});
+	}
+
+	ll ans = 1010;
+
+	for (auto aa: possa) {
+		for (auto bb: possb) {
+			ll quads[4] = {0};
+
+			for (ll i = 0; i < N; i++) {
+				ll x = cows[i].f;
+				ll y = cows[i].s;
+
+				if (x > aa && y > bb) {
+					quads[0]++;
+				}
+				else if (x < aa && y > bb) {
+					quads[1]++;
+				}
+				else if (x < aa && y < bb) {
+					quads[2]++;
+				}
+				else {
+					quads[3]++;
+				}
+			}
+
+			ll M = 0;
+			for (ll i = 0; i < 4; i++) {
+				if (quads[i] > M) {
+					M = quads[i];
+				}
+			}
+
+			cout << M << " ";
+
+			if (M < ans) {
+				ans = M;
+			}
+		}
+	}
+
+	cout << "\n";
+
+	cout << ans << "\n";
+
+	return 0;
+}
