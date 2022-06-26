148669489afdbafa5d7f48d6f3b02f64e7914a7a
chenant2017 <chenant2017@gmail.com>
2022-06-26 12:19:09 -0700

    Create invite.cpp
    8/10 TLE for 8 and 9

---
+++ cpp_learning/invite.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define MAXN 1000010
+#define MAXG 250010
+
+using namespace std;
+
+ll N, G;
+vector<ll> cow_group[MAXN];
+vector<ll> group_cow[MAXG];
+bool visited[MAXN] = {false};
+bool included[MAXG] = {false};
+
+ll get_cows(ll start) {
+	queue<ll> cowq;
+	cowq.push(start);
+	ll result = 0;
+
+	while (!cowq.empty()) {
+		ll curr = cowq.front();
+		cowq.pop();
+
+		if (visited[curr]) {
+			continue;
+		}
+		visited[curr] = true;
+		result++;
+
+		for (auto i: cow_group[curr]) {\
+			if (included[i]) {
+				continue;
+			}
+
+			vector<ll> rem;
+			for (auto j: group_cow[i]) {
+				if (!visited[j]) {
+					rem.push_back(j);
+				}
+			}
+			if (rem.size() <= 1) {
+				included[i] = true;
+				if (rem.size() == 1) {
+					cowq.push(rem[0]);
+				}
+			}
+		}
+	}	
+	return result;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "invite";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N >> G;
+
+
+	for (ll i = 0; i < G; i++) {
+		ll size;
+		cin >> size;
+		for (ll j = 0; j < size; j++) {
+			ll a;
+			cin >> a;
+			cow_group[a].push_back(i);
+			group_cow[i].push_back(a);
+		}
+	}
+
+	cout << get_cows(1) << "\n";
+
+	return 0;
+}
