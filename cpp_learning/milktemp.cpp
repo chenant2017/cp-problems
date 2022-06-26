c1bdd8ab0f66026767d6e692d3fe65f795f23d62
chenant2017 <chenant2017@gmail.com>
2022-06-26 16:11:00 -0700

    Update milktemp.cpp
    10/10
    
    Previous logic relied on the assumption that ALL cows overlapped somewhere which is very unliikely

--- cpp_learning/milktemp.cpp
+++ cpp_learning/milktemp.cpp
@@ -31,10 +31,11 @@ int main() {
 	}
 
 	unordered_set<ll> overlap;
-
-	ll max_overlap = 0;
-	ll lower = 1000000010;
-	ll upper = -1;
+	ll too_cold = N;
+	ll just_right = 0;
+	ll too_hot = 0;
+	
+	ll ans = 0;
 
 	for (auto& i: startend) {
 		ll temp = i.f;
@@ -44,61 +45,27 @@ int main() {
 		for (auto j: to_process) {
 			if (overlap.find(j) == overlap.end()) {
 				overlap.insert(j);
+				too_cold--;
+				just_right++;
 			}
 			else {
 				to_remove.push_back(j);
 			}
 		}
 
-		/*cout << temp << " is temp\n";
-		for (auto k: overlap) {
-			cout << k << " ";
-		}
-		cout << "\n";*/
-
-		if (overlap.size() > max_overlap) {
-			lower = temp;
-			upper = temp;
-			max_overlap = overlap.size();
-		}
-		else if (overlap.size() == max_overlap) {
-			if (temp < lower) {
-				lower = temp;
-			}
-			if (temp > upper) {
-				upper = temp;
-			}
+		ll milk = X * too_cold + Y * just_right + Z * too_hot;
+		if (milk > ans) {
+			ans = milk;
 		}
 
 		for (auto j: to_remove) {
 			overlap.erase(j);
-		}
-	}
-
-	ll optimal;
-
-	if (X > Z) {
-		optimal = lower;
-	}
-	else {
-		optimal = upper;
-	}
-
-	ll ans = 0;
-	for (ll i = 0; i < N; i++) {
-		if (optimal < ranges[i].f) {
-			ans += X;
-		}
-		else if (optimal > ranges[i].s) {
-			ans += Z;
-		}
-		else {
-			ans += Y;
+			too_hot++;
+			just_right--;
 		}
 	}
 
 	cout << ans << "\n";
-
 
 	return 0;
 }
