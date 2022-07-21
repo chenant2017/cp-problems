14ee1a4ee640ebdf0b19ef9bdc86770bad3d24da
chenant2017 <chenant2017@gmail.com>
2022-07-20 21:26:08 -0400

    Create abc.cpp
    AC
    
    This problem can be seen as a complete search problem; you just need to narrow down what you are searching.
    
    In this case, you should search the pairwise differences among the given numbers because A, B, and C all MUST be a difference between two of the given numbers.
    
    This observation is actually relatively straightforward if you think about it.

---
+++ cpp_learning/abc.cpp
@@ -0,0 +1,66 @@
+//2021 Silver Open #2
+
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	//string fname = "abc";
+	//freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+
+	ll T;
+	ll N;
+	
+	cin >> T;
+
+	for (ll i = 0; i < T; i++) {
+		ll numbers[10];
+		cin >> N;
+		set<ll> diffs;
+
+		for (ll j = 0; j < N; j++) {
+			cin >> numbers[j];
+		}
+		numbers[N] = 0;
+	
+		for (ll j = 0; j < N; j++) {
+			for (ll k = j + 1; k < N + 1; k++) {
+				if (numbers[j] != numbers[k]) {
+					diffs.insert(abs(numbers[j] - numbers[k]));
+				}
+			}
+		}
+
+		ll ans = 0;
+
+		for (auto a: diffs) {
+			for (auto b: diffs) {
+				for (auto c: diffs) {
+					if (a <= b && b <= c) {
+						set<ll> all_numbers({a, b, c, a + b, b + c, a + c, a + b + c});
+						bool works = true;
+						
+						for (ll k = 0; k < N; k++) {
+							if (all_numbers.find(numbers[k]) == all_numbers.end()) {
+								works = false;
+							}
+						}
+
+
+						if (works)  {
+							ans++;
+						}
+					}
+				}
+			}
+		}
+		cout << ans << "\n";
+	}
+	return 0;
+}
