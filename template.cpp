e0817b1926c947cfdfef90a1da1fecd478ef02fd
chenant2017 <chenant2017@gmail.com>
2022-08-02 00:15:10 -0400

    Clean up

--- template.cpp
+++ template.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 typedef long long ll;
-
-using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string fname = "";
+	string fname = "1714A";
 	freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	
 	
 	return 0;
 }
