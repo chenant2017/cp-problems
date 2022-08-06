9101face5fb321bf43cd0e89b5a790927a7ac722
chenant2017 <chenant2017@gmail.com>
2022-08-06 15:06:12 -0400

    Create gifts.cpp

---
+++ Bronze/2012 Jan/gifts.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define MAXN 1010
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+ll N, B;
+pll costs[MAXN];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "gifts";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N >> B;
+	
+	for (ll i = 0; i < N; i++) {
+		cin >> costs[i].f >> costs[i].s;
+	}
+	
+	ll ans = -1;
+	for (ll disc = 0; disc < N; disc++) {
+		priority_queue<ll, vector<ll>, greater<ll>> cheapest;
+		for (ll i = 0; i < N; i++) {
+			if (i == disc) {
+				cheapest.push(costs[i].f / 2 + costs[i].s);
+			}
+			else {
+				cheapest.push(costs[i].f + costs[i].s);
+			}
+		}
+
+		ll count = 0;
+		ll cost = 0;
+		while (!cheapest.empty() && cost + cheapest.top() <= B){
+			cost += cheapest.top();
+			cheapest.pop();
+			count++;
+		}
+
+		if (count > ans) {
+			ans = count;
+		}
+	}
+
+	cout << ans << "\n";
+
+	return 0;
+}
