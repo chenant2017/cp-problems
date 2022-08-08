b4e19cfb628266ea52bd1161f7f48871b5383c12
chenant2017 <chenant2017@gmail.com>
2022-08-08 19:53:18 -0400

    Create soulmates2.cpp
    AC
    
    Find all the values that a and b take as operations are applied to them until they are 1. Then, find the shortest path from a to b using these lists of values.

---
+++ cpp_learning/soulmates2.cpp
@@ -0,0 +1,81 @@
+//Silver 2022 Jan #1
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+ll N;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "soulmates";
+	//freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N;
+
+	for (ll i = 0; i < N; i++) {
+		ll a, b;
+		cin >> a >> b;
+
+		if (a == b)  {
+			cout << "0\n";
+			continue;
+		}
+
+		vector<ll> seqa, seqb;
+
+		seqa.push_back(a);
+		seqb.push_back(b);
+
+		while (a > 1) {
+			if (a % 2 == 0) {
+				a /= 2;
+			}
+			else {
+				a++;
+			}
+			seqa.push_back(a);
+		}
+
+		while (b > 1) {
+			if (b % 2 == 0) {
+				b /= 2;
+			}
+			else {
+				b--;
+			}
+			seqb.push_back(b);
+		}
+
+		ll ptra = 0;
+		ll ptrb = 0;
+
+		ll ans = -1;
+
+		while (seqa[ptra] > seqb[0]) {
+			ptra++;
+		}
+
+		while (ptra < seqa.size()) {
+			while (ptrb < seqb.size() && 
+				   seqb[ptrb + 1] >= seqa[ptra]) {
+				ptrb++;
+			}
+			ll moves = ptra + ptrb + seqb[ptrb] - seqa[ptra];
+			if (moves < ans || ans == -1) {
+				ans = moves;
+			}
+			ptra++;
+		}
+		
+		cout << ans << "\n";
+	}
+	
+	return 0;
+}
