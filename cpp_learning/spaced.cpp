3779b4330210c22e74446b2a19738d4f9a4a1164
chenant2017 <chenant2017@gmail.com>
2022-08-16 12:02:28 -0700

    Update spaced.cpp
    AC
    
    A placement of cows is valid if and only if each row alternates between cow and no cow OR each column alternates between cow and no cow

--- cpp_learning/spaced.cpp
+++ cpp_learning/spaced.cpp
@@ -1,102 +1,39 @@
 #include <bits/stdc++.h>
 #define MAX 1010
-#define f first
-#define s second
 using namespace std;
 
 typedef long long ll;
-
 ll N;
+
 ll beauty[MAX][MAX];
 
-ll dr[4] = {0, 1, 0, 1};
-ll dc[4] = {0, 0, 1, 1};
-
-ll cr1[6] = {0, 0, 1, 1, 1, 1};  
-ll cc1[6] = {0, 0, 0, 0, 0, 1};
-ll cr2[6] = {1, 0, 0, 1, 0, 0};
-ll cc2[6] = {1, 1, 0, 1, 1, 1};
-
-pair<ll, ll> get_next(ll i, ll j) {
-	pair<ll, ll> result;
-	if (j < N - 2) {
-		result.f = i;
-		result.s = j + 1;
-	}	
-	else {
-		result.f = i + 1;
-		result.s = 0;
+ll max_sum_row(ll r) {
+	ll sum1 = 0;
+	for (ll i = 0; i < N; i += 2) {
+		sum1 += beauty[r][i];
 	}
-	return result;
+
+	ll sum2 = 0;
+	for (ll i = 1; i < N; i += 2) {
+		sum2 += beauty[r][i];
+	}
+
+	return max(sum1, sum2);
 }
 
-bool has_2(vector<vector<bool>> next, ll i, ll j) {
-	//cout << i << " " << j << "\n";
-	if (i == -1 || j == -1) return true;
-	ll count = 0;
-	for (ll d = 0; d < 4; d++) {
-		if (next[i + dr[d]][j + dc[d]]) {
- 			count++;
- 		}
-	} 
-	return (count == 2);
+ll max_sum_col(ll c) {
+	ll sum1 = 0;
+	for (ll i = 0; i < N; i += 2) {
+		sum1 += beauty[i][c];
+	}
+
+	ll sum2 = 0;
+	for (ll i = 1; i < N; i += 2) {
+		sum2 += beauty[i][c];
+	}
+
+	return max(sum1, sum2);
 }
-
- ll solve(ll i, ll j, vector<vector<bool>> curr) {
- 	//cout << i << " " << j << " A\n";
- 	if (i == N - 1) {
- 		ll lala = 0;
- 		for (ll p = 0; p < N; p++) {
- 			for (ll q = 0; q < N; q++) {
- 				//cout << curr[p][q] << " ";
- 				if (curr[p][q]) {
- 					lala += beauty[p][q];
- 				}
- 			}
- 			//cout << "\n";
- 		}
- 		//cout << "lala is " << lala << "\n";
- 		return 0;
- 	}
-
- 	ll ans = 0;
- 	ll old = 0;
- 	
- 	for (ll d = 0; d < 4; d++) {
- 		if (curr[i + dr[d]][j + dc[d]]) {
- 			old++;
- 		}
- 	}
-
- 	for (ll x = 0; x < 6; x++) {
- 		ll b = 0;
- 		ll novel = 0;
- 		auto next = curr;
- 		if (!curr[i + cr1[x]][j + cc1[x]]) {
- 			next[i + cr1[x]][j + cc1[x]] = true; 
- 			novel++;
- 			b += beauty[i + cr1[x]][j + cc1[x]];
- 		}
- 		if (!curr[i + cr2[x]][j + cc2[x]]) {
- 			next[i + cr2[x]][j + cc2[x]] = true; 
- 			novel++;
- 			b += beauty[i + cr2[x]][j + cc2[x]];
- 		}
-
- 		if (old + novel == 2 && 
- 			has_2(next, i - 1, j) &&
- 			has_2(next, i, j - 1)) { //must check above and right squares also ok
- 			auto p2 = get_next(i, j);
- 			ll s = solve(p2.f, p2.s, next);
- 			b += s;
- 			if (b > ans) {
-	 			ans = b;
-	 		}
- 		}
- 	}
-
- 	return ans;
- }
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -114,18 +51,17 @@ int main() {
 		}
 	}
 
-	vector<vector<bool>> start;
-	for (ll i = 0; i < N; i++) {
-		vector<bool> v;
-		for (ll j = 0; j < N; j++) {
-			v.push_back(false);
-		}
-		start.push_back(v);
+	ll totalr = 0;
+	for (ll r = 0; r < N; r++) {
+		totalr += max_sum_row(r);
 	}
 
-	ll ans = solve(0, 0, start);
+	ll totalc = 0;
+	for (ll c = 0; c < N; c++) {
+		totalc += max_sum_col(c);
+	}
 
-	cout << ans << "\n";
+	cout << max(totalr, totalc) << "\n";
 
 	return 0;
 }
