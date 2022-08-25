ae98fd7d8c83d0bb17c80e3e741ebd58c4b3b8d9
chenant2017 <chenant2017@gmail.com>
2022-08-24 21:04:16 -0700

    One-character "blacklist" does not improve

--- cpp_learning/subset.cpp
+++ cpp_learning/subset.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
+#define MAX 100010
 using namespace std;
 
 typedef long long ll;
 
 ll Q;
 string s, t;
+ll s_counts[26] = {0};
+ll t_counts[26] = {0};
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -15,12 +18,30 @@ int main() {
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
     cin >> s >> t >> Q;
+
+    for (char c: s) {
+        s_counts[c - 'a']++;
+    }
+    for (char c: t) {
+        t_counts[c - 'a']++;
+    }
 	
     for (ll i = 0; i < Q; i++) {
         string q;
         cin >> q;
         unordered_set<char> letters;
-        for (char c: q) letters.insert(c);
+        bool works = true;
+        for (char c: q) {
+            if (s_counts[c - 'a'] != t_counts[c - 'a']) {
+                works = false;
+                break;
+            }
+            letters.insert(c);
+        }
+        if (!works) {
+            cout << "N";
+            continue;
+        }
         string s_mod = string();
         for (char c: s) {
             if (letters.find(c) != letters.end()) s_mod += c;
