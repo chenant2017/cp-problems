5bb461f10bcd69594e9344f1dfd8bf732bb86fd0
chenant2017 <chenant2017@gmail.com>
2022-08-28 20:06:35 -0700

    10/10
    BFS from two points concurrently
    
    If there were K exits, this would reduce the complexity from MNK to MN

--- alphastar/gold_basics/mansion.cpp
+++ alphastar/gold_basics/mansion.cpp
@@ -12,22 +12,23 @@ typedef tuple<ll, ll, ll> tll;
 
 ll W, H;
 string maze[MAXH];
-vector<pll> exits;
-ll exit1[MAXH][MAXW] = {{0}};
-ll exit2[MAXH][MAXW] = {{0}};
+vector<pll> distss;
+ll dists[MAXH][MAXW] = {{0}};
 ll di[4] = {0, 0, -1, 1};
 ll dj[4] = {1, -1, 0, 0};
+vector<ll> starts;
 
-void bfs(ll i, ll j, ll exit[MAXH][MAXW]) {
+void bfs() {
     queue<tll> q;
-    q.push(tll({i, j, 1}));
+    q.push(tll({starts[0], starts[1], 1}));
+    q.push(tll({starts[2], starts[3], 1}));
 
     while (!q.empty()) {
         ll a, b, c;
         tie(a, b, c) = q.front();
         q.pop();
-        if (exit[a][b] != 0) continue;
-        exit[a][b] = c;
+        if (dists[a][b] != 0) continue;
+        dists[a][b] = c;
 
         for (ll d = 0; d < 4; d++) {
             ll nexta = a + di[d];
@@ -37,7 +38,7 @@ void bfs(ll i, ll j, ll exit[MAXH][MAXW]) {
 
             if (nexta2 >= 0 && nexta2 <= 2 * H &&
                 nextb2 >= 0 && nextb2 <= 2 * W &&
-                exit[nexta2][nextb2] == 0 && 
+                dists[nexta2][nextb2] == 0 && 
                 maze[nexta][nextb] == ' ' &&
                 maze[nexta2][nextb2] == ' ') {
                 q.push(tll({nexta2, nextb2, c + 1}));
@@ -51,7 +52,7 @@ int main() {
 	cin.tie(NULL);
 	
 	string fname = "mansion";
-	//freopen((fname + ".in").c_str(), "r", stdin);
+	freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
 	cin >> W >> H;
@@ -65,48 +66,45 @@ int main() {
 
     for (ll i = 1; i <= 2 * W; i += 2) {
         if (maze[0][i] == ' ') {
-            exits.push_back(pll({0, i}));
+            distss.push_back(pll({0, i}));
         }
         if (maze[2 * H][i] == ' ') {
-            exits.push_back(pll({2 * H, i}));
+            distss.push_back(pll({2 * H, i}));
         }
     }
 
     for (ll i = 1; i <= 2 * H; i += 2) {
         if (maze[i][0] == ' ') {
-            exits.push_back(pll({i, 0}));
+            distss.push_back(pll({i, 0}));
         }
         if (maze[i][2 * W] == ' ') {
-            exits.push_back(pll({i, 2 * W}));
+            distss.push_back(pll({i, 2 * W}));
         }
     }
 
     for (ll e = 0; e < 2; e++) {
         for (ll d = 0; d < 4; d++) {
-            ll starti = exits[e].f + di[d];
-            ll startj = exits[e].s + dj[d];
+            ll starti = distss[e].f + di[d];
+            ll startj = distss[e].s + dj[d];
             if (starti >= 0 && starti <= 2 * H &&
                 startj >= 0 && startj <= 2 * W &&
                 maze[starti][startj] == ' ') {
 
-                if (e == 0) {
-                    bfs(starti, startj, exit1);
-                }
-                else {
-                    bfs(starti, startj, exit2);
-                }
+                starts.push_back(starti);
+                starts.push_back(startj);
 
                 break;
             }
         }
     }
 
+    bfs();
+
     ll ans = 0;
 
     for (ll i = 0; i <= 2 * H; i++) {
         for (ll j = 0; j <= 2 * W; j++) {
-            ll dist = min(exit1[i][j], exit2[i][j]);
-            ans = max(dist, ans);
+            ans = max(dists[i][j], ans);
         }
     }
 	
