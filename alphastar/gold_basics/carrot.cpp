d47162e100ece94698eebd4393ddd02ecd6f27ce
chenant2017 <chenant2017@gmail.com>
2022-10-14 10:50:23 -0700

    1/10

--- alphastar/gold_basics/carrot.cpp
+++ alphastar/gold_basics/carrot.cpp
@@ -10,19 +10,12 @@ typedef tuple<ll, ll, ll> tll;
 ll N, T;
 ll sizes[MAXN][MAXN];
 ll times[MAXN][MAXN];
-ll destsi[6] = {N - 3, N - 2, N - 2, N - 1, N - 1, N - 1};
-ll destsj[6] = {N - 1, N - 1, N - 2, N - 1, N - 2, N - 3};
+ll destsi[6] = {-3, -2, -2, -1, -1, -1};
+ll destsj[6] = {-1, -1, -2, -1, -2, -3};
 bool visited[MAXN][MAXN] = {{false}};
 
-void reset() {
-    for (ll i = 0; i < N; i++) {
-        for (ll j = 0; j < N; j++) {
-            times[i][j] = pow(10, 7);
-        }
-    }
-}
 
-ll dijkstra() {
+void dijkstra() {
     priority_queue<tll, vector<tll>, greater<tll>> q;
     q.push(tll({0, 0, 0}));
 
@@ -33,13 +26,18 @@ ll dijkstra() {
 
         if (visited[i][j]) continue;
         visited[i][j] = true;
+        times[i][j] = t;
+
+       // cout << i << " " << j << " start\n";
 
         for (ll di = 0; di <= 3; di++) {
             ll dj = 3 - di;
             for (ll si = -1; si <= 1; si += 2) {
-                for (ll sj = -1; sj <= 1; sj++) {
+                for (ll sj = -1; sj <= 1; sj+= 2) {
                     ll nexti = i + di * si;
-                    ll nextj = j + dj * dj;
+                    ll nextj = j + dj * sj;
+
+                   // cout << nexti << " " << nextj << "\n";
 
                     if (0 <= nexti && nexti < N &&
                         0 <= nextj && nextj < N &&
@@ -60,6 +58,8 @@ ll dijkstra() {
                 }
                 ll nexti = i + di;
                 ll nextj = j + dj;
+
+                //cout << nexti << " " << nextj << "\n";
 
                 if (0 <= nexti && nexti < N &&
                     0 <= nextj && nextj < N &&
@@ -90,15 +90,23 @@ int main() {
         }
     }
 
-    reset();
+    for (ll i = 0; i < N; i++) {
+        for (ll j = 0; j < N; j++) {
+            times[i][j] = pow(10, 7);
+        }
+    }
+
     dijkstra();
 
     ll ans = pow(10, 7);
 
     for (ll x = 0; x < 6; x++) {
-        ll desti = destsi[x];
-        ll destj = destsj[x];
-        ans = min(ans, times[desti][destj]);
+        ll desti = N + destsi[x];
+        ll destj = N + destsj[x];
+        ll dest_dist = abs(desti - (N - 1)) + abs(destj - (N - 1));
+        //cout << desti << " " << destj << " sdaf\n";
+        //cout << times[desti][destj] << " sdlfk\n";
+        ans = min(ans, times[desti][destj] + T * dest_dist);
     }
     
     cout << ans << "\n";
