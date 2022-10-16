8b76950abe5c5ca4c1415f9a3204aa45698e8664
chenant2017 <chenant2017@gmail.com>
2022-10-16 11:44:01 -0700

    2/14

---
+++ alphastar/gold_basics/GA1103.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#define MAX 35
+using namespace std;
+
+typedef long long ll;
+typedef tuple<ll, ll, ll> ttll;
+typedef tuple<ll, ll, ll, ll, ll> tll; 
+
+ll M, N;
+ll pond[MAX][MAX];
+ttll dists[MAX][MAX];
+bool visited[MAX][MAX] = {false};
+ll di[] = {1, 2};
+ll dj[] = {2, 1};
+
+void dijkstra(ll starti, ll startj) {
+    ll min_geese = M * N;
+    
+    priority_queue<tll, vector<tll>, greater<tll>> q;
+    q.push(tll({0, 0, starti, startj, 1}));
+    dists[starti][startj] = ttll({0, 0, 1});
+
+    while (!q.empty()) {
+        ll geese, jumps, i, j, ways;
+        tie(geese, jumps, i, j, ways) = q.top();
+        q.pop();
+
+        if (visited[i][j]) continue;
+        visited[i][j] = true;
+        //cout << i << " " << j << "\n";
+        
+        for (ll d = 0; d < 2; d++) {
+            for (ll si = -1; si <= 1; si += 2) {
+                for (ll sj = -1; sj <= 1; sj += 2) {
+                    ll ni = i + di[d] * si;
+                    ll nj = j + dj[d] * sj;
+
+                    if (0 <= ni && ni < M &&
+                        0 <= nj && nj < N) {
+                        ll ngeese, njumps, nways;
+                        tie(ngeese, njumps, nways) = dists[ni][nj];
+                        if (pond[ni][nj] == 0) geese++;
+                        if (geese < ngeese) {
+                            ngeese = geese;
+                            njumps = jumps + 1;
+                            nways = ways;
+                        }
+                        else if (geese == ngeese) {
+                            if (njumps == jumps + 1) {
+                                //cout << nways << " nways before\n";
+                                nways += ways;
+                            }
+                            else if (jumps + 1 < njumps) {
+                                njumps = jumps + 1;
+                                nways = ways;
+                            }
+                        }
+                        if (pond[ni][nj] == 0) geese--;
+                        dists[ni][nj] = ttll({ngeese, njumps, nways});
+                        q.push(tll({ngeese, njumps, ni, nj, nways}));
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "GA1103";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> M >> N;
+
+    ll starti, startj;
+    ll desti, destj;
+
+    for (ll i = 0; i < M; i++) {
+        for (ll j = 0; j < N; j++) {
+            cin >> pond[i][j];
+            if (pond[i][j] == 3) {
+                starti = i;
+                startj = j;
+            }
+            if (pond[i][j] == 4) {
+                desti = i;
+                destj = j;
+            }
+        }
+    }
+
+    for (ll i = 0; i < M; i++) {
+        for (ll j = 0; j < N; j++) {
+            dists[i][j] = ttll({pow(10, 4), pow(10, 4), 0});
+        }
+    }
+
+    dijkstra(starti, startj);
+
+    for (ll i = 0; i < M; i++) {
+        for (ll j = 0; j < N; j++) {
+            ll a, b, c;
+            tie(a, b, c) = dists[i][j];
+            //cout << a << ", " << b << ", " << c << "   ";
+        }
+        //cout << "\n";
+    }
+
+    ll ans1, ans2, ans3;
+
+    tie(ans1, ans2, ans3) = dists[desti][destj];
+
+    cout << ans1 << "\n";
+    cout << ans2 << "\n";
+    cout << ans3 << "\n";
+	
+	return 0;
+}
