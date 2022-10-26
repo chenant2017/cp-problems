f188ace8c31c846939251a6e51bdb13aa74448d2
chenant2017 <chenant2017@gmail.com>
2022-10-25 20:35:48 -0700

    original

---
+++ fixing2.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define MAXN 
+#define f first
+#define s second
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+bool visited[MAXN];
+
+ll N, C;
+pll points[MAXN];
+
+ll get_dist(ll a, ll b) {
+    ll x1, y1;
+    tie(x1, y1) = points[a];
+    ll x2, y2;
+    tie(x2, y2) = points[b];
+    return pow(x1 - x2, 2) + pow(y1 - y2, 2);
+}
+
+ll prims() {
+    ll result = 0;
+    ll reached = 0;
+
+    priority_queue<pll, vector<pll>, greater<pll>> q;
+
+    while (!q.empty()) {
+        auto curr = q.top();
+        q.pop();
+
+        if (visited[curr.s]) continue;
+
+        result += curr.f;
+        visited[curr.s] = true;
+        reached++;
+
+        for (ll i = 0; i < N; i++) {
+            if (visited[i]) continue;
+            ll dist = get_dist(curr.s, i);
+            if (dist >= C) {
+                q.push(pll({get_dist(curr.s, i), i}));
+            }
+        }
+    }
+
+    if (reached == N) return result;
+    return -1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "fixing2";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N >> C;
+
+    for (ll i = 0; i < N; i++) {
+        cin >> points[i].f >> points[i].s;
+    }
+	
+    cout << prims() << "\n";
+
+	return 0;
+}
