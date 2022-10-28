92cc8c7b002e9f5f9a2e6834abb4d0af0b8eb479
chenant2017 <chenant2017@gmail.com>
2022-10-27 21:08:25 -0700

    AC rewrite

--- alphastar/gold_basics/jumping.cpp
+++ alphastar/gold_basics/jumping.cpp
@@ -1,83 +1,56 @@
 #include <bits/stdc++.h>
-#define MAXN 310
 #define f first 
-#define s second 
-
+#define s second
+#define MAXN 310
 using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 
 ll N, M, T;
+
 vector<pll> adj[MAXN];
-ll shortest[MAXN][MAXN];
-
-void fw() {
-    for (ll i = 1; i <= N; i++) {
-        for (ll j = 1; j <= N; j++) {
-            shortest[i][j] = pow(10, 7);
-        }
-    }
-    for (ll i = 1; i <= N; i++) {
-        for (auto j: adj[i]) {
-            shortest[i][j.f] = j.s;
-        }
-        shortest[i][i] = 0;
-    }
-
-    /*for (ll i = 1; i <= N; i++) {
-        for (ll j = 1; j <= N; j++) {
-            cout << shortest[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    cout << "\n";*/
-
-    for (ll k = 1; k <= N; k++) { //intermediate node
-        for (ll i = 1; i <= N; i++) {
-            for (ll j = 1; j <= N; j++) {
-                shortest[i][j] = min(shortest[i][j], 
-                                     max(shortest[i][k], shortest[k][j]));
-            }
-        }
-    }
-}
+ll shortests[MAXN][MAXN] = {0};
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string fname = "jumping";
+	string fname = "GA0903";
 	freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
 	cin >> N >> M >> T;
 
-	for (ll i = 0; i < M; i++) {
-		ll st, e, h;
-		cin >> st >> e >> h;
-
-        adj[st].push_back(pll({e, h}));
-	}
-
-    fw();
-
-    /*for (ll i = 1; i <= N; i++) {
+    for (ll i = 1; i <= N; i++) {
         for (ll j = 1; j <= N; j++) {
-            cout << shortest[i][j] << " ";
+            if (i != j) {
+                shortests[i][j] = pow(10, 7);
+            }
         }
-        cout << "\n";
-    }*/
+    }
 
+    ll s, e, h;
+    for (ll i = 0; i < M; i++) {
+        cin >> s >> e >> h;
+        adj[s].push_back(pll({e, h}));
+        shortests[s][e] = h;
+    }
+
+    for (ll k = 1; k <= N; k++) {
+        for (ll i = 1; i <= N; i++) {
+            for (ll j = 1; j <= N; j++) {
+                ll min2 = max(shortests[i][k], shortests[k][j]);
+                shortests[i][j] = min(shortests[i][j], min2);
+            }
+        }
+    }
+
+    ll a, b;
     for (ll i = 0; i < T; i++) {
-        ll a, b;
         cin >> a >> b;
-        if (shortest[a][b] == pow(10, 7)) {
-            cout << "-1\n";
-        }
-        else {
-            cout << shortest[a][b] << "\n";
-        }
+        if (shortests[a][b] == pow(10, 7)) cout << "-1\n";
+        else cout << shortests[a][b] << "\n";
     }
 	
 	return 0;
