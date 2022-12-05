2ea64b2d971cbef3b0bfe17baa44cc8ffd8819b1
chenant2017 <chenant2017@gmail.com>
2022-12-04 17:04:13 -0800

    Did not convert from 0-indexing to 1-indexing for answer

--- leftout.cpp
+++ leftout.cpp
@@ -1,34 +1,41 @@
 #include <bits/stdc++.h>
 #define MAX 1010
 #define f first  
-#define s second  
-
+#define s second
 using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 
 ll N;
-string cows[MAX];
+ll cows[MAX][MAX];
 
-bool unsolvable(ll i, ll j) {
-    ll rcount = 0;
-    ll lcount = 0;
-
-    for (ll di = 0; di < 2; di++) {
-        for (ll dj = 0; dj < 2; dj++) {
-            ll curri = i + di;
-            ll currj = j + dj;
-
-            if (cows[curri][currj] == 'R') {
-                rcount++;
-            }
-            else {
-                lcount++;
-            }
-        }
+void rflip(ll r) {
+    for (ll i = 0; i < N; i++) {
+        cows[r][i] = 1 - cows[r][i];
     }
-    return rcount == 1 || lcount == 1;
+}
+
+void cflip(ll c) {
+    for (ll i = 0; i < N; i++) {
+        cows[i][c] = 1 - cows[i][c];
+    }
+}
+
+ll rsum(ll r) {
+    ll result = 0;
+    for (ll i = 0; i < N; i++) {
+        result += cows[r][i];
+    }
+    return result;
+}
+
+ll csum(ll c) {
+    ll result = 0;
+    for (ll i = 0; i < N; i++) {
+        result += cows[i][c];
+    }
+    return result;
 }
 
 int main() {
@@ -42,28 +49,86 @@ int main() {
 	cin >> N;
 
     for (ll i = 0; i < N; i++) {
-        cin >> cows[i];
+        string s;
+        cin >> s;
+        for (ll j = 0; j < N; j++) {
+            cows[i][j] = s[j] == 'R';
+        }
+    }
+
+    for (ll i = 1; i < N; i++) {
+        if (cows[i][0] != cows[0][0]) {
+            rflip(i);
+        }
+        if (cows[0][i] != cows[0][0]) {
+            cflip(i);
+        }
+    }
+
+    if (cows[0][0] == 1) {
+        for (ll i = 0; i < N; i++) {
+            rflip(i);
+        }
+    }
+
+    ll total = 0;
+    for (ll i = 0; i < N; i++) {
+        for (ll j = 0; j < N; j++) {
+            total += cows[i][j];
+        }
+    }
+    if (total == pow(N - 1, 2)) {
+        cout << "1 1" << "\n";
+        return 0;
     }
 
     pll ans = pll({-1, -1});
-    ll unsolvables = 0;
-
-    for (ll i = 0; i < N - 1; i++) {
-        for (ll j = 0; j < N - 1; j++) {
-            if (unsolvable(i, j)) {
-                if (ans == pll({-1, -1})) {
-                    ans = pll({i, j});
-                }
-                unsolvables++;
+    
+    for (ll i = 1; i < N; i++) {
+        if (rsum(i) == N - 1) {
+            if (ans == pll({-1, -1})) {
+                ans = pll({i, 0});
+            }
+            else {
+                cout << "-1\n";
+                return 0;
+            }
+        }
+        if (csum(i) == N - 1) {
+            if (ans == pll({-1, -1})) {
+                ans = pll({0, i});
+            }
+            else {
+                cout << "-1\n";
+                return 0;
             }
         }
     }
 
-    if (unsolvables == 1) {
+    if (ans != pll({-1, -1})) {
         cout << ans.f + 1 << " " << ans.s + 1 << "\n";
+        return 0;
     }
-    else  {
+
+    for (ll i = 1; i < N; i++) {
+        for (ll j = 1; j < N; j++) {
+            if (cows[i][j] == 1) {
+                if (ans == pll({-1, -1})) {
+                    ans = pll(i, j);
+                }
+                else {
+                    cout << "-1\n";
+                    return 0;
+                }
+            }
+        }
+    }
+
+    if (ans == pll({-1, -1})) {
         cout << "-1\n";
+    }
+    else {
+        cout << ans.f + 1 << " " << ans.s + 1 << "\n";
     }
 	
 	return 0;
