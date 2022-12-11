3be3f28e16d0bc3267d4d4aa73428fda4006f570
chenant2017 <chenant2017@gmail.com>
2022-12-10 18:35:41 -0800

    10/12

--- herding.cpp
+++ herding.cpp
@@ -1,5 +1,3 @@
-//Silver 2019 Feb #1
-
 #include <bits/stdc++.h>
 #define MAXN 100010
 using namespace std;
@@ -9,20 +7,24 @@ typedef long long ll;
 ll N;
 ll cows[MAXN];
 
-map<ll, ll> prefix;
-
-void adjust(ll low, ll high, ll& min_occupied, ll& max_occupied) {
-    if (high <= cows[N - 1]) {
-        auto low_ptr = prefix.lower_bound(low);
-        auto high_ptr = prefix.upper_bound(high);
-        high_ptr--;
-
-        ll occupied = high_ptr->second - low_ptr->second + 1;
-        //cout << occupied << "a\n";
-
-        min_occupied = min(min_occupied, occupied);
-        max_occupied = max(max_occupied, occupied);
+ll solve_min() {
+    ll ptr2 = 0;
+    ll max_cows = 0;
+    for (ll ptr1 = 0; ptr1 < N; ptr1++) {
+        if (cows[ptr1] + N - 1 > cows[N - 1]) break;
+        while (ptr2 + 1 < N && cows[ptr2 + 1] - cows[ptr1] < N) {
+            ptr2++;
+        }
+        ll num_cows = ptr2 - ptr1 + 1;
+        max_cows = max(max_cows, num_cows);
     }
+    return N - max_cows;
+}
+
+ll solve_max() {
+    ll poss1 = cows[N - 1] - cows[1];
+    ll poss2 = cows[N - 2] - cows[0];
+    return max(poss1, poss2) + 2 - N;
 }
 
 int main() {
@@ -31,7 +33,7 @@ int main() {
 	
 	string fname = "herding";
 	freopen((fname + ".in").c_str(), "r", stdin);
-	freopen((fname + ".out").c_str(), "w", stdout);
+	//freopen((fname + ".out").c_str(), "w", stdout);
 	
 	cin >> N;
 
@@ -40,26 +42,9 @@ int main() {
     }
 
     sort(cows, cows + N);
-    
-    cows[N] = pow(10, 10);
 
-    for (ll i = 0; i < N; i++) {
-        prefix[cows[i]] = i + 1;
-    }
-
-    ll min_occupied = N;
-    ll max_occupied = 0;
-
-    //cout << N << "\n";
-
-    for (ll i = 0; i < N; i++) {
-        ll high = cows[i] + N - 1;
-        adjust(cows[i], high, min_occupied, max_occupied);
-        adjust(cows[i] + 1, high + 1, min_occupied, max_occupied);
-    }
-
-    cout << N - max_occupied << "\n";
-    cout << N - min_occupied << "\n";
+    cout << solve_min() << "\n";
+    cout << solve_max() << "\n";
 	
 	return 0;
 }
