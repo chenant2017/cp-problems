31143adab0fae18f4b1ff69abf4a64470fe1461d
chenant2017 <chenant2017@gmail.com>
2022-12-11 17:12:19 -0800

    update A/C

--- closest.cpp
+++ closest.cpp
@@ -14,12 +14,33 @@ pll patches[MAX];
 ll nhoj[MAX];
 priority_queue<ll> tasties;
 
+ll get_max_intersect(ll nhoj1, ll nhoj2, ll patch1, ll patch2) {
+    vector<pll> endpoints;
+    for (ll p = patch1; p <= patch2; p++) {
+        ll r1 = patches[p].f - nhoj[nhoj1];
+        ll r2 = nhoj[nhoj2] - patches[p].f;
+        
+        endpoints.push_back(pll({patches[p].f - min(r1, r2), patches[p].s}));
+        endpoints.push_back(pll({patches[p].f + min(r1, r2), -patches[p].s}));
+    }
+
+    ll result = 0;
+    ll sum = 0;
+    sort(endpoints.begin(), endpoints.end());
+    for (auto i: endpoints) {
+        sum += i.s;
+        result = max(sum, result);
+    }
+
+    return result;
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string fname = "1714A";
-	freopen((fname + ".in").c_str(), "r", stdin);
+	string fname = "closest";
+	//freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
 	cin >> K >> M >> N;
@@ -35,20 +56,51 @@ int main() {
     sort(patches, patches + K);
     sort(nhoj, nhoj + M);
 
-    ll j = -1;   
+    ll j = 0;
 
-    for (ll i = 0; i < K; i++) {
-        ll total = 0;
+    for (ll i = 0; i < M; i++) { //nhoj
+        if (j >= K) break;
+        if (nhoj[i] < patches[j].f) continue;
+
         ll j2 = j;
-        while (j2 + 1 < K && patches[j2 + 1].f < nhoj[i]) {
-            j2++;
+        ll total = 0;
+        while (j2 < K && patches[j2].f < nhoj[i]) {
             total += patches[j2].s;
+            j2++;
         }
-        ll max_intersect = get_max_intersect(j, j2);
-        tasties.push(max_intersect);
-        tasties.push(total - max_intersect);
-        j = j2;
+
+        //cout << "cows " << i - 1 << " " << i << " patches " << j << " " << j2 << "\n";
+        if (i == 0) {
+            tasties.push(total);
+            //cout << "total " << total << "\n";
+        }
+        else {
+            ll max_intersect = get_max_intersect(i - 1, i, j, j2 - 1);
+            tasties.push(max_intersect);
+            tasties.push(total - max_intersect);
+        }
+        j = j2; //out of range
     }
+
+    //MUST ADD RIGHTMOST PART OF PATCHES
+    ll tasty = 0;
+    //cout << j << " j\n";
+    while (j < K) {
+        tasty += patches[j].s; 
+        j++;
+    }
+    //cout << tasty << "asdf\n";
+    tasties.push(tasty);
+
+    ll taken = 0;
+    ll ans = 0;
+    while (!tasties.empty() && taken < N) { 
+        ans += tasties.top();
+        //cout << tasties.top() << "\n";
+        tasties.pop();
+        taken++;
+    }
+    cout << ans << "\n";
 	
 	return 0;
 }
