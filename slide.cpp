a87e71f53588a3c1a2b989f885ac1a8e84ed057f
chenant2017 <chenant2017@gmail.com>
2022-12-23 21:04:51 -0800

    AC
    Bottom-up approach with top sort

--- slide.cpp
+++ slide.cpp
@@ -10,40 +10,48 @@ typedef pair<ll, ll> pll;
 
 ll V, E, K;
 vector<pll> adj[MAXV];
+vector<ll> tsorted;
+bool visited[MAXV];
 ll dp[MAXV][MAXK];
-bool ready[MAXV][MAXK] = {false};
 
-ll get_dp(ll i, ll j) {
-    if (ready[i][j]) return dp[i][j];
-    
-    if (i == V) {
-        dp[i][j] = 0;
-        ready[i][j] = true;
-        return dp[i][j];
+void solve_dp() {
+    for (ll i = 0; i <= K; i++) {
+        dp[tsorted[0]][i] = 0;
     }
 
-    ll success = 0;
-    ll fail = -1;
-    for (auto a: adj[i]) {
-        ll s = a.s + get_dp(a.f, j);
-        success = max(success, s);
+    for (ll i = 1; i < V; i++) {
+        for (ll j = 0; j <= K; j++) {
 
-        if (j > 0) {
-            ll f = a.s + get_dp(a.f, j - 1);
-            if (fail == -1 || f < fail) fail = f;
+            ll success = 0;
+            ll fail = -1;
+            for (auto& a: adj[tsorted[i]]) {
+                ll s = a.s + dp[a.f][j];
+                success = max(success, s);
+                if (j > 0) {
+                    ll f = a.s + dp[a.f][j - 1];
+                    if (fail == -1 || f < fail) fail = f;
+                }
+            }
+
+            if (j == 0) {
+                dp[tsorted[i]][j] = success;
+            }
+            else {
+                dp[tsorted[i]][j] = min(success, fail);
+            }
         }
     }
+}
 
-    if (j == 0) {
-        dp[i][j] = success;
-        ready[i][j] = true;
-        return dp[i][j];
+void tsort(ll start) {
+    if (visited[start]) return;
+    visited[start] = true;
+
+    for (auto i: adj[start]) {
+        if (!visited[i.f]) tsort(i.f);
     }
 
-
-    dp[i][j] = min(success, fail);
-    ready[i][j] = true;
-    return dp[i][j];
+    tsorted.push_back(start);
 }
 
 int main() {
@@ -62,7 +70,11 @@ int main() {
         adj[p].push_back(pll({q, f}));
     }
 
-    cout << get_dp(1, K) << "\n";
+    tsort(1); 
+    
+    solve_dp();
+
+    cout << dp[1][K] << "\n";
 	
 	return 0;
 }
