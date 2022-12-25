de61fbe541465c5fefab5c199419db8161506dd4
chenant2017 <chenant2017@gmail.com>
2022-12-25 15:12:08 -0800

    AC

--- fenceplan.cpp
+++ fenceplan.cpp
@@ -1,33 +1,30 @@
 #include <bits/stdc++.h>
-#define MAXN 100010
 #define f first 
-#define s second
+#define s second 
+#define MAXN 100010
 using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 
 ll N, M;
-pll points[MAXN];
+pll cows[MAXN];
 vector<ll> adj[MAXN];
 bool visited[MAXN] = {false};
 
-void dfs(ll& minx, ll& miny, ll& maxx, ll& maxy, ll start) {
-    if (visited[start]) return;
-    visited[start] = true;
+void floodfill(ll i, vector<ll>& extrema) { // minx, maxx, miny, maxy
+    if (visited[i]) return;
+    visited[i] = true;
 
-    minx = min(minx, points[start].f);
-    maxx = max(maxx, points[start].f);
-    miny = min(miny, points[start].s);
-    maxy = max(maxy, points[start].s);
+    extrema[0] = min(extrema[0], cows[i].f);
+    extrema[1] = max(extrema[1], cows[i].f);
+    extrema[2] = min(extrema[2], cows[i].s);
+    extrema[3] = max(extrema[3], cows[i].s);
 
-    for (auto i: adj[start]) {
-        if (!visited[i]) {
-            dfs(minx, miny, maxx, maxy, i);
-        }
+    for (auto j: adj[i]) {
+        if (!visited[j]) floodfill(j, extrema);
     }
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -39,34 +36,30 @@ int main() {
 	
 	cin >> N >> M;
 
-    for (ll i = 0; i < N; i++) {
-        cin >> points[i].f >> points[i].s;
+    for (ll i = 1; i <= N; i++) {
+        cin >> cows[i].f >> cows[i].s;
     }
-	
-    for (ll i = 0; i < M; i++) {
+
+    for (ll i = 1; i <= M; i++) {
         ll a, b;
         cin >> a >> b;
-
-        a--;
-        b--;
-
         adj[a].push_back(b);
-        adj[b].push_back(a);    
+        adj[b].push_back(a);
     }
-	
-    
-    ll ans = 4 * pow(10, 9);
 
-    for (ll i = 0; i < N; i++) {
+    ll ans = 4e9;
+
+    for (ll i = 1; i <= N; i++) {
         if (!visited[i]) {
-            ll minx = pow(10, 9), miny = pow(10, 9), maxx = -1, maxy = -1;
-            dfs(minx, miny, maxx, maxy, i);
+            vector<ll> extrema = {(ll) 1e9, -1, (ll) 1e9, -1};
+            floodfill(i, extrema);
 
-            ans = min(ans, 2 * (maxx + maxy - (minx + miny)));
+            ll perimeter = 2 * (extrema[1] + extrema[3] - extrema[0] - extrema[2]);
+            ans = min(perimeter, ans);
         }
     }
 
     cout << ans << "\n";
-
+	
 	return 0;
 }
