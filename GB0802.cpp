9dceba251e59021c3e3c5cdef7111863fcbfdf06
chenant2017 <chenant2017@gmail.com>
2022-12-26 11:04:17 -0800

    AC
    Find "cycles"

--- GB0802.cpp
+++ GB0802.cpp
@@ -4,9 +4,16 @@
 using namespace std;
 
 typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<pll, pll> ppll;
+
+map<pll, ll> counts;
 
 ll N;
-ll line[MAXN];
+
+ll original[MAXN];
+ll sorted[MAXN];
+ppll complements[] = {{{1, 2}, {2, 1}}, {{2, 3}, {3, 2}}, {{1, 3}, {3, 1}}};
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -19,27 +26,38 @@ int main() {
 	cin >> N;
 
     for (ll i = 0; i < N; i++) {
-        cin >> line[i];
+        ll a;
+        cin >> a;
+        original[i] = a;
+        sorted[i] = a;
+    }
+
+    sort(sorted, sorted + N);
+
+    for (ll i = 0; i < N; i++) {
+        //cout << original[i] << " " << sorted[i] << "\n";
+        if (original[i] != sorted[i]) {
+            counts[pll({original[i], sorted[i]})]++;
+        }
     }
 
     ll ans = 0;
+    ll total = 0;
 
-    for (ll i = 0; i < N - 1; i++) {
-        ll smallest = N + 1;
-        ll partner;
-        for (ll j = i + 1; j < N; j++) {
-            if (line[j] < smallest) {
-                partner = j;
-                smallest = line[j];
-            }
-        }
-        if (line[i] != line[partner]) {
-            swap(line[i], line[partner]);
-            ans++;
-        }
+    for (auto i: complements) {
+        ll min_count = min(counts[i.first], counts[i.second]);
+        counts[i.first] -= min_count;
+        counts[i.second] -= min_count;
+        ans += min_count;
+        total += counts[i.first] + counts[i.second];
     }
 
+    //cout << total << "\n";
+
+    ans += ((double)2/3) * total;
+
     cout << ans << "\n";
+    
 	
 	return 0;
 }
