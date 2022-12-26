a3acfc300103350ddb205eac189b4af06342a291
chenant2017 <chenant2017@gmail.com>
2022-12-25 16:03:37 -0800

    3/12

--- cowjump.cpp
+++ cowjump.cpp
@@ -6,13 +6,18 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
+typedef pair<pll, ll> ppll;
 
 struct Segment {
     pll p1, p2;
 };
 
 ll N;
+ll currx;
 Segment segs[MAXN];
+vector<ppll> endpoints;
+pll ans = {-1, -1};
+
 
 ll sign(ll a) {
     if (a > 0) return 1;
@@ -25,7 +30,7 @@ ll cp(pll a, pll b) {
 }
 
 pll diff(pll a, pll b) {
-    return pll({a.x - b.x, a.y - b.y});
+    return {a.x - b.x, a.y - b.y};
 }
 
 bool intersect(Segment s1, Segment s2) {
@@ -36,6 +41,37 @@ bool intersect(Segment s1, Segment s2) {
     ll cp4 = cp(diff(s2.p1, s1.p2), diff(s2.p2, s1.p2));
 
     return (sign(cp1) * sign(cp2) <= 0 && sign(cp3) * sign(cp4) <= 0);
+}
+
+double gety(Segment s) {
+    if (s.p2.x == s.p1.x) return s.p2.y;
+    double m = (double) (s.p2.y - s.p1.y) / (s.p2.x - s.p1.x);
+    return s.p1.y + m * (currx - s.p1.x);
+}
+
+bool cmp(const ll& a, const ll& b) {
+    return gety(segs[a]) < gety(segs[b]);
+}
+
+bool update_ans(ll a, ll b) {
+    if (ans.first == -1) {
+        ans = {a, b};
+        return false;
+    }
+    
+    else {
+        if (a == ans.first || a == ans.second) {
+            if (b == ans.first || b == ans.second) {
+                return false;
+            }
+            cout << a + 1 << "\n";
+            return true;
+        }
+        else {
+            cout << b + 1 << "\n";
+            return true;
+        }
+    }
 }
 
 int main() {
@@ -56,28 +92,71 @@ int main() {
         }
     }
 
-    ll ans = -1;
+    for (ll i = 0; i < N; i++) {
+
+    }
 
     for (ll i = 0; i < N; i++) {
-        
-        ll intersects = 0;
-        for (ll j = 0; j < N; j++) {
-            if (i == j) continue;
-            if (intersect(segs[i], segs[j])) {
-                intersects++;
-            }
-        }
+        ppll start = {segs[i].p1, i};
+        ppll end = {segs[i].p2, i};
 
-        if (intersects > 1) {
-            cout << i + 1 << "\n";
-            return 0;
+        endpoints.push_back(start);
+        endpoints.push_back(end);
+    }
+
+    sort(endpoints.begin(), endpoints.end());
+
+    set<ll, decltype(&cmp)> active(cmp);
+
+    
+    for (auto e: endpoints) {
+        if (active.find(e.second) == active.end()) {
+
+            active.insert(e.second);
+            auto it = active.find(e.second);
+
+            auto it2 = it;
+            it2++;
+            if (it2 != active.end()) {
+                if (intersect(segs[*it], segs[*it2])) {
+                    if (update_ans(*it, *it2)) {
+                        return 0;
+                    }
+                }
+            }
+
+            if (it != active.begin()) {
+                it2 = it;
+                it2--;
+                if (intersect(segs[*it], segs[*it2])) {
+                    if (update_ans(*it, *it2)) {
+                        return 0;
+                    }
+                }
+            }
+
         }
-        else if (intersects == 1) {
-            if (ans == -1) ans = i + 1;
+        else {
+
+            auto it = active.find(e.second);
+            auto it2 = it;
+            auto it3 = it;
+
+            it2++;
+            it3--;
+
+            if (it2 != active.end() && it != active.begin()) {
+                if (intersect(segs[*it2], segs[*it3])) {
+                    if (update_ans(*it2, *it3)) {
+                        return 0;
+                    }
+                }
+            }
+
         }
     }
 
-    cout << ans << "\n";
+    cout << min(ans.first, ans.second) + 1 << "\n";
 
 	return 0;
 }
