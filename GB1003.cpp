cb3d4a0819cf8a0b2ddec8c1a6e7f36f98759dc8
chenant2017 <chenant2017@gmail.com>
2022-12-30 12:25:08 -0800

    1/12

--- GB1003.cpp
+++ GB1003.cpp
@@ -1,57 +1,59 @@
 #include <bits/stdc++.h>
 #define MAXN 410
+
 using namespace std;
 
 typedef long long ll;
 
-ll N, K;
 ll snakes[MAXN];
-ll last_max[MAXN][MAXN][MAXN];
-ll dp[MAXN][MAXN][MAXN]; //index, nets, size of last group of groups
+ll N, K;
+ll dp[MAXN][MAXN];
+ll groups[MAXN][MAXN];
+ll sizes[MAXN][MAXN];
 
 void solve_dp() {
     for (ll i = 0; i <= N; i++) {
-        for (ll nets = 0; nets <= N; nets++) {
-            for (ll last = 0; last <= N; last++)  {
-                dp[i][nets][last] = 1e10;
-            }
-        }
+        dp[i][0] = 1e10;
+        groups[i][0] = 1e10;
+        sizes[i][0] = -1;
+
+        dp[1][i] = 1e10;
+        groups[1][i] = 1e10;
+        sizes[1][i] = -1;
     }
+    dp[1][1] = 0;
+    groups[1][1] = 1;
+    sizes[1][1] = snakes[1];
 
-    dp[0][1][1] = 0;
-    last_max[0][1][1] = snakes[0];
+    for (ll i = 2; i <= N; i++) {
+        for (ll nets = 1; nets <= K + 1; nets++) {
+            ll dp1 = dp[i - 1][nets - 1];
+            ll groups1 = 1;
+            ll sizes1 = snakes[i];
 
-    for (ll i = 1; i < N; i++) {
-        dp[i][1][i + 1] = dp[i - 1][1][i];
-        if (last_max[i - 1][1][1] >= snakes[i]) {
-            last_max[i][1][i + 1] = last_max[i - 1][1][i];
-            dp[i][1][i + 1] += last_max[i - 1][1][i] - snakes[i];
-        }
-        else {
-            last_max[i][1][i + 1] = snakes[i];
-            dp[i][1][i + 1] += i * (snakes[i] - last_max[i - 1][1][i]);
-        }
-        
 
-        for (ll nets = 2; nets <= K; nets++) {
-            //last = 1
+            ll dp2 = dp[i - 1][nets];
+            ll groups2 = groups[i - 1][nets] + 1;
+            ll sizes2;
 
-            dp[i][nets][1] = 1e10;
-            for (ll j = 1; j <= i - 1; j++) {
-                dp[i][nets][1] = min(dp[i][nets][1], dp[i - 1][nets - 1][j]);
+            if (snakes[i] > sizes[i - 1][nets]) {
+                sizes2 = snakes[i];
+                dp2 += groups[i - 1][nets] * (snakes[i] - sizes[i - 1][nets]);
             }
-            cout << i << " " << nets << " " << dp[i][nets][1] << " a\n";
+            else {
+                sizes2 = sizes[i - 1][nets];
+                dp2 += sizes[i - 1][nets] - snakes[i];
+            }
 
-            for (ll last = 2; last <= N; last++) {
-                dp[i][nets][last] = dp[i][nets][last - 1];
-                if (last_max[i][nets][last - 1] >= snakes[i]) {
-                    last_max[i][nets][last] = last_max[i][nets][last - 1];
-                    dp[i][nets][last] += last_max[i][nets][last] - snakes[i];
-                }
-                else {
-                    last_max[i][nets][last] = snakes[i];
-                    dp[i][nets][last] += (last - 1) * (snakes[i] - last_max[i][nets][last - 1]);
-                }
+            if (dp1 > dp2) {
+                dp[i][nets] = dp2;
+                groups[i][nets] = groups2;
+                sizes[i][nets] = sizes2;
+            }
+            else {
+                dp[i][nets] = dp1;
+                groups[i][nets] = groups1;
+                sizes[i][nets] = sizes1;
             }
         }
     }
@@ -67,21 +69,13 @@ int main() {
 	
 	cin >> N >> K;
 
-    for (ll i = 0; i < N; i++) {
-        cin >> snakes[i];
+    for (ll i = 1; i <= N; i++) {
+        cin >> snakes[i];        
     }
 
     solve_dp();
 
-    for (ll i = 0; i <= N; i++) {
-        for (ll j = 0; j <= K; j++) {
-            for (ll k = 0; k <= N; k++) {
-                cout << dp[i][j][k] << " ";
-            }
-            cout << "\n";
-        }
-        cout << "\n\n";
-    }
+    cout << dp[N][K + 1] << "\n";
 	
 	return 0;
 }
