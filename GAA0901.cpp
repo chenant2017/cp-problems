952e8a89e01c13f66003cf5d52cb704524ff857b
chenant2017 <chenant2017@gmail.com>
2023-01-21 14:14:40 -0800

    7/10 WA

---
+++ GAA0901.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define MAXN 100010
+using namespace std;
+
+typedef long long ll;
+
+ll N, M;
+ll daily[MAXN];
+
+bool works(ll maxe) {
+    ll sum = 0;
+    ll count = 0;
+
+    for (ll i = 0; i < N; i++) {
+        if (sum + daily[i] <= maxe) {
+            sum += daily[i];
+        }
+        else {
+            count++;
+            sum = daily[i];
+        }
+    }
+
+    if (sum > 0) count++;
+
+    return count <= M;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "GAA0901";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N >> M;
+
+    for (ll i = 0; i < N; i++) {
+        cin >> daily[i];
+    }
+
+    ll low = 0;
+    ll high = 1e6 + 5;
+
+    while (high - low > 1) {
+        ll mid = (low + high) / 2;
+        if (works(mid)) {
+            high = mid;
+        }
+        else {
+            low = mid;
+        }
+    }
+
+    cout << high << "\n";
+	
+	return 0;
+}
