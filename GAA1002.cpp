ac5bf59ccc222c406d1de69821db053e25ba22ff
chenant2017 <chenant2017@gmail.com>
2023-01-22 15:49:51 -0800

    AC

--- GAA1002.cpp
+++ GAA1002.cpp
@@ -8,9 +8,9 @@ ll N;
 vector<ll> barn (MAXN);
 ll pf[MAXN];
 vector<ll> rotated;
-ll source[MAXN];
+ll source2[MAXN];
 
-ll get_source(ll i) {
+ll get_source(ll i, ll* source) {
     ll s = source[i];
     while (barn[s] == 0) {
         s = source[s];
@@ -18,44 +18,7 @@ ll get_source(ll i) {
     return s;
 }
 
-int main() {
-	//ios_base::sync_with_stdio(false);
-	//cin.tie(NULL);
-	
-	string fname = "GAA1002";
-	freopen((fname + ".in").c_str(), "r", stdin);
-	//freopen((fname + ".out").c_str(), "w", stdout);
-	
-	cin >> N;
-
-    for (ll i = 0; i < N; i++) {
-        cin >> barn[i];
-    }
-
-    ll split = N - 1;
-
-    pf[N - 1] = barn[N - 1];
-    for (ll i = N - 2; i >= 0; i--) {
-        pf[i] = pf[i + 1] + barn[i];
-        //cout << "i " << i << " " << pf[i] << "\n";
-        if (barn[i] == 0 && pf[i + 1] >= N - i - 1) {
-            split = i;
-            break;
-        }
-    }
-
-   // cout << "split " << split << "\n";
-
-    for (ll i = split + 1; i < N; i++) {
-        rotated.push_back(barn[i]);
-    }
-
-    for (ll i = 0; i <= split; i++) {
-        rotated.push_back(barn[i]);
-    }
-
-    barn = rotated;
-
+void get_sources(ll* source) {
     ll ptr1 = 0;
     ll ptr2 = 0;
 
@@ -72,13 +35,57 @@ int main() {
             ptr1 = ptr2;
         }
     }
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "GAA1002";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N;
+
+    for (ll i = 0; i < N; i++) {
+        cin >> barn[i];
+    }
+
+    ll split = N - 1;
+
+    pf[N] = 0;
+
+    for (ll i = N - 1; i >= 0; i--) {
+        pf[i] = pf[i + 1] + barn[i] - 1;
+    }
+
+    ll maxpf = -1e18;
+
+    for (ll i = N - 1; i >= 0; i--) {
+        if (barn[i] == 0 && pf[i] > maxpf) {
+            maxpf = pf[i];
+            split = i;
+        }
+    }
+
+    for (ll i = split + 1; i < N; i++) {
+        rotated.push_back(barn[i]);
+    }
+
+    for (ll i = 0; i <= split; i++) {
+        rotated.push_back(barn[i]);
+    }
+
+    barn = rotated;
+
+    get_sources(source2);
 
     ll ans = 0;
 
     for (ll i = N - 1; i >= 0; i--) {
         if (barn[i] == 1) continue;
         assert(barn[i] == 0);
-        ll s = get_source(i);
+        ll s = get_source(i, source2);
         barn[s]--;
         barn[i]++;
         ans += pow(s - i, 2);
