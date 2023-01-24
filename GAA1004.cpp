3ffb9ed76f3219715ebc7698012304d85ff7348a
chenant2017 <chenant2017@gmail.com>
2023-01-23 20:41:31 -0800

    AC

--- GAA1004.cpp
+++ GAA1004.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 #define MAXN 100010
-#define f first 
-#define s second
+
 using namespace std;
 
 typedef long long ll;
-typedef pair<ll, ll> pll;
 
-vector<ll> adj[MAXN];
-bool visited[MAXN];
-ll leaves[MAXN];
-ll dists[MAXN];
+ll N, L;
 ll lengths[MAXN];
-ll ans[MAXN];
-
-ll N;
+vector<ll> adj[MAXN];
+ll leaves[MAXN];
+ll down[MAXN];
+ll up[MAXN];
+bool visited[MAXN];
 
 ll get_leaves(ll i) {
-    if (visited[i]) return 0;
-    visited[i] = true;
-
-    if (adj[i].size() == 1) {
+    if (adj[i].empty() == 1) {
         leaves[i] = 1;
         return 1;
     }
@@ -28,46 +22,32 @@ ll get_leaves(ll i) {
     ll result = 0;
 
     for (auto j: adj[i]) {
-        result += get_leaves(j.s);
+        result += get_leaves(j);
     }
+
     leaves[i] = result;
+
     return result;
 }
 
-void dijkstra(ll i) {
-    for (ll i = 1; i <= N; i++) {
-        dists[i] = 1e18;
-    }
-
-    priority_queue<pll, vector<pll>, greater<pll>> pq;
-    pq.push({0, 1});
-    dists[1] = 0;
-
-    while (!pq.empty()) {
-        auto c = pq.top().s;
-        pq.pop();
-
-        if (visited[c]) continue;
-        visited[c] = true;
-
-        for (auto i: adj[c]) {
-            if (dists[i.s] > dists[c] + i.f) {
-                dists[i.s] = dists[c] + i.f;
-                pq.push({dists[i.s], i.s});
-            }
-        }
-    }
-}
-
-void dfs(ll i) {
-    if (visited[i]) return;
-    visited[i] = true;
+ll get_down(ll i) {
+    ll result = 0;
 
     for (auto j: adj[i]) {
-        ans[j.s] = ans[i] + j.f * leaves[i] - 3 * leaves[j.s];
-        dfs(j.s);
+        result += get_down(j) + leaves[j] * lengths[j];
     }
-}   
+
+    down[i] = result;
+    return result;
+}
+
+ll get_up(ll i) {
+    for (auto j: adj[i]) {
+        up[j] = up[i] + 3 * (L - leaves[j]) + down[i] - down[j] - lengths[j] * leaves[j];
+        get_up(j);
+    }
+    return 0;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -77,41 +57,33 @@ int main() {
 	freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
-    cin >> N;
+	cin >> N;
 
     for (ll i = 1; i <= N; i++) {
-        string name; cin >> name;
+        string name;
+        cin >> name;
+        lengths[i] = name.size() + 1;
 
-        cout << name << " " << name.size() << "\n";
-
-        lengths[i] = name.size();
-
-        ll n; cin >> n;
-        for (ll j = 0; j < n; j++) {
-            ll k; cin >> k;
-            adj[i].push_back({k});
-            adj[k].push_back({i});
+        ll m; cin >> m;
+        for (ll j = 0; j < m; j++) {
+            ll a; cin >> a;
+            adj[i].push_back(a);
         }
     }
 
-    for (ll i = 1; i <= N; i++) {
-        for (auto j: adj[i]) {
-            cout << j.f << " " << j.s << "    ";
-        }
-        cout << "\n";
-    }
+    L = get_leaves(1);
+    get_down(1);
+    get_up(1);
 
-    get_leaves(1);
+
+    ll ans = 1e18;
 
     for (ll i = 1; i <= N; i++) {
-        visited[i] = false;
+        if (adj[i].empty()) continue;
+        ans = min(ans, up[i] + down[i]);
     }
 
-    dijkstra(1);
-
-    for (ll i = 1; i <= N; i++) {
-        cout << dists[i] << "\n";
-    }
+    cout << ans - L << "\n";
 
 	
 	return 0;
