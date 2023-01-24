46bafc7091824ba789e15f1aff2ae6f001006c87
chenant2017 <chenant2017@gmail.com>
2023-01-23 21:40:33 -0800

    4/10

--- GAA1003.cpp
+++ GAA1003.cpp
@@ -1,76 +1,70 @@
 #include <bits/stdc++.h>
-#define MAXN 
-#define f first 
-#define s second
+#define MAXN 210
+#define x first 
+#define y second
 using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 
-double dist(pll a, pll b) {
-    return sqrt(pow(a.f - b.f, 2) + pow(a.s - b.s, 2));
-}
-
-double dot(pll a, pll b, pll c) {
-    pll p = {b.f - a.f, b.s - a.s};
-    pll q = {c.f - b.f, c.s - b.s};
-
-    return p.f * q.f + p.s * q.s;
-}
-
-struct Vt {
-    ll x, y, de;
-    double dr, dl;
-    double angle;
-
-    Vt(ll i) {
-        pll pl = pts[(i - 1 + N) % N];
-        pll pr = pts[(i + 1 + N) % N];
-
-        dl = dist(pl, pts[i]);
-        dr = dist(pr, pts[i]); 
-
-        de = (0 - i + N) % N;
-        
-        angle = dot(pl, pts[i], pr);
-        tie(x, y) = pts[i];
-    }
-};
-
 ll N;
 pll pts[MAXN];
-Vt vertices[MAXN];
-set<Vt> vts;
+vector<double> v;
+multiset<vector<double>> subs;
+vector<double> cw;
+vector<double> ccw;
 
+double dist(pll a, pll b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
 
-ll moves(ll i) {0
-    ll j = i;
+double angle(pll a, pll b, pll c) {
+    pll u = {a.x - b.x, a.y - b.y};
+    pll v = {c.x - b.x, c.y - b.y};
 
-    ll min_de = 0;
-    
-    double dl = -1;
+    double dot = u.x * v.x + u.y * v.y;
+    double umag = dist(a, b);
+    double vmag = dist(b, c);
 
-    while (true) {
-        double angle = vertices[j].angle;
+    return acos(dot / (umag * vmag));
+}
 
-        set<Vt> vts2 = vts;
-        vector<Vt> rm;
-        for (auto v: vts2) {
-            if (v.de < min_de || v.angle != angle) {
-                rm.push_back(v);
+void get_subs() {
+    for (ll i = 0; i < v.size(); i++) {
+        for (ll j = i; j < v.size(); j++) {
+            vector<double> s;
+            for (ll k = i; k <= j; k++) {
+                s.push_back(v[k]);
             }
+            subs.insert(s);
         }
-        for (auto v: rm) {
-            vts2.erase(v);
-        }
-
-        if (vts2.size() == 1) {
-            return (j - i); 
-        }
-
-        j = (j + 1) % N;
     }
-    return -1;
+}
+
+void get_ccw() {
+    ccw.resize(v.size());
+
+    ccw[0] = v[0];
+    
+    for (ll i = 1; i < v.size(); i++) {
+        ccw[i] = ccw[i - 1];
+        if (v[i] > 0) {
+            ccw[i] += v[i];
+        }
+    }
+}
+
+void get_cw() {
+    cw.resize(v.size());
+
+    cw[v.size() - 1] = 0;
+
+    for (ll i = v.size() - 2; i >= 0; i--) {
+        cw[i] = cw[i + 1];
+        if (v[i] > 0) {
+            cw[i] += v[i];
+        }
+    }
 }
 
 int main() {
@@ -84,19 +78,49 @@ int main() {
 	cin >> N;
 
     for (ll i = 0; i < N; i++) {
-        cin >> pts[i].f >> pts[i].s;
+        cin >> pts[i].x >> pts[i].y;
     }
 
     for (ll i = 0; i < N; i++) {
-        Vt vt = Vt(i);
-        vts.insert(vt);
-        vertices[i] = vt;
+        double d = dist(pts[i], pts[(i + 1) % N]);
+        double a = -1 * angle(pts[i], pts[(i + 1) % N], pts[(i + 2) % N]);
+        if (i == N - 1) {
+            a = -10;
+        }
+        v.push_back(d);
+        v.push_back(a);
     }
 
-    for (ll i = 0; i < N; i++) {
-        ll mv = moves(i);
+    get_subs();
+    get_ccw();
+    get_cw();
 
+    double ans = 0;
+
+    for (ll i = 1; i < v.size(); i += 2) {
+        vector<double> t;
+
+        double orig = min(cw[i], ccw[i]);
+
+        double dist2 = 0;
+
+        ll j = i;
+
+        for (; j < v.size(); j += 2) {
+            t.push_back(v[j]);
+            if (subs.count(t) == 1) {
+                break;
+            }
+            t.push_back(v[j + 1]);
+            dist2 += v[j + 1];
+        }
+
+        dist2 += min(cw[j], ccw[j]);
+
+        ans = max(ans, dist2 - orig);
     }
+
+    cout << ans << "\n";
 	
 	return 0;
 }
