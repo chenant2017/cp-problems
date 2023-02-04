0c62eff19ef21e280896dec30487e4e6ee77d3b4
chenant2017 <chenant2017@gmail.com>
2023-02-04 14:29:44 -0800

    3/10

--- GAA1101.cpp
+++ GAA1101.cpp
@@ -1,152 +1,78 @@
 #include <bits/stdc++.h>
-#define x first 
-#define f first
+#define MAX 1010
+#define f first 
 #define s second
-#define y second
-#define MAXN 1010
 using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 
-ll N, C;
-
 struct Seg {
-    pll p1, p2;
+	pll p1, p2;
+	Seg(pll a, pll b) {
+		p1 = a;
+		p2 = b;
+		if (p1 > p2) swap(p1, p2);
+	}
 
-    Seg(pll a, pll b) {
-        p1 = a;
-        p2 = b;
-    }
+	bool operator<(const Seg& se) const {
+		return pair<pll, pll>({p1, p2}) < pair<pll, pll>({se.p1, se.p2});
+	}
 };
 
-struct Poly {
-    vector<Seg> segs;
-    ll minx = 1e7, maxx = -1, miny = 1e7, maxy = -1;
-
-    void add(Seg a) {
-        segs.push_back(a);
-
-        minx = min(minx, a.p1.x);
-        minx = min(minx, a.p2.x);
-        maxx = max(maxx, a.p1.x);
-        maxx = max(maxx, a.p2.x);
-
-        miny = min(miny, a.p1.y);
-        miny = min(miny, a.p2.y);
-        maxy = max(maxy, a.p1.y);
-        maxy = max(maxy, a.p2.y);
-    }
-};
-
-map<pll, ll> comp;
+ll N, C;
 map<pll, vector<pll>> adj;
-vector<vector<ll>> poly_adj;
-vector<vector<ll>> poly_par; //parent
-vector<ll> points_in;
-vector<pll> pts;
-vector<Poly> polys;
-vector<bool> visited;
+map<pll, ll> comp;
+set<Seg> polys [MAX];
+map<vector<ll>, ll> ans;
 
-void get_comp(pll p, ll c) { // dfs
-    if (comp[p] != -1) return;
-    comp[p] = c;
-
-    for (auto i: adj[p]) {
-        get_comp(i, c);
-    }
+void get_comps(pll p, ll c) {
+	if (comp[p] != 0) return;
+	comp[p] = c;
+	for (auto i: adj[p]) {
+		polys[c].insert(Seg(p, i));
+		if (comp[i] == 0) {
+			get_comps(i, c);
+		}	
+	}
 }
 
 ll cp(pll a, pll b) {
-    ll result = a.x * b.y - a.y * b.x;
-    if (result > 0) {
-        return 1;
-    }
-    else if (result == 0) {
-        return 0;
-    }
-    else {
-        return -1;
-    }
+	return a.f * b.s - b.f * a.s;
 }
 
 pll diff(pll a, pll b) {
-    return {a.x - b.x, a.y - b.y};
+	return {a.f - b.f, a.s - b.s};
 }
 
 bool intersect(Seg a, Seg b) {
-    pll v1 = diff(a.p2, a.p1);
-    pll v2 = diff(b.p1, a.p1);
-    pll v3 = diff(b.p2, a.p1);
+	pll v1, v2, v3;
+	v1 = diff(a.p2, a.p1);
+	v2 = diff(b.p1, a.p1);
+	v3 = diff(b.p2, a.p1);
 
-    bool cond = (cp(v1, v2) * cp(v1, v3) <= 0);
+	bool b1 = cp(v1, v2) * cp(v1, v3) < 0;
 
-    v1 = diff(b.p1, b.p2);
-    v2 = diff(a.p1, b.p1);
-    v3 = diff(a.p2, b.p1);
+	v1 = diff(b.p2, b.p1);
+	v2 = diff(a.p1, b.p1);
+	v3 = diff(a.p2, b.p1);
 
-    cond = cond && (cp(v1, v2) * cp(v1, v3) <= 0);
+	bool b2 = cp(v1, v2) * cp(v1, v3) < 0;
 
-    return cond;
+	return b1 && b2;
 }
 
-bool pip(pll a, Poly p) {
-    set<ll> xs;
+bool pip(pll p, ll po) {
+	ll count = 0;
+	Seg ray = Seg(p, {p.f + 1, p.s + 1e7});
+	for (auto i: polys[po]) {
+		if (intersect(ray, i)) {
+			count++;
+		}
+	}
 
-    ll count = 0;
-
-    Seg line = {a, {1e7, a.y}};
-
-    for (auto i: p.segs) {
-        if (intersect(line, i)) {
-            if (i.p1.y == a.y) {
-                if (xs.find(i.p1.x) == xs.end()) {
-                    xs.insert(i.p1.x);
-                    count++;
-                }
-            } 
-            else if(i.p2.s == a.y) {
-                if (xs.find(i.p2.x) == xs.end()) {
-                    xs.insert(i.p2.x);
-                    count++;
-                }
-            }
-            else {
-                count++;
-            }
-        }
-    }
-    
-    return (count % 2 == 1);
+	return count % 2 == 1;
 }
-
-bool contains(Poly a, Poly b) {
-    return a.minx < b.minx && a.maxx > b.maxx &&
-            a.miny < b.miny && a.maxy > b.maxy;
-}
-
-void get_points_in(ll i) {
-    if (visited[i]) return;
-    visited[i] = true;
-
-    ll result = 0;
-
-    for (auto p: pts) {
-        if (pip(p, polys[i])) {
-            result++;
-        }
-    }
-
-    for (auto j: poly_adj[i]) {
-        if (poly_par[j].size() == 1) {
-            get_points_in(j);
-            result -= points_in[j];
-        }
-    }
-
-    points_in[i] = result;
-}
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -156,69 +82,46 @@ int main() {
 	freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
-	cin >> N >> C;
+    cin >> N >> C;
 
     for (ll i = 0; i < N; i++) {
-        pll p1, p2;
-        cin >> p1.x >> p1.y;
-        cin >> p2.x >> p2.y;
-
-        if (p1 > p2) swap(p1, p2);
-
-        adj[p1].push_back(p2);
-
-        comp[p1] = -1;
-        comp[p2] = -1;
+        ll x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+		adj[{x1, y1}].push_back({x2, y2});
+		adj[{x2, y2}].push_back({x1, y1});
     }
 
-    ll comps = 0;
+	ll c = 0;
+	for (auto i: adj) {
+		if (comp[i.f] == 0) {
+			c++;
+			get_comps(i.f, c);
+		}
+	}
 
-    for (auto i: adj) {
-        if (comp[i.f] == -1) {
-            get_comp(i.f, comps);
-            comps++;
-        }
-    }
+	for (ll i = 0; i < C; i++) {
+		pll p;
+		cin >> p.f >> p.s;
 
-    polys.resize(comps);
-    poly_adj.resize(comps);
-    poly_par.resize(comps);
-    visited.resize(comps);
-    points_in.resize(comps);
+		vector<ll> v;
+		for (ll j = 1; j <= c; j++) {
+			if (pip(p, j)) {
+				v.push_back(j);
+			}
+		}
 
-    for (auto i: adj) {
-        for (auto j: i.s) {
-            polys[comp[i.f]].add(Seg(i.f, j));
-        }
-    }
+		ans[v]++;
+	}
 
-    for (ll i = 0; i < comps; i++) {
-        for (ll j = 0; j < comps; j++) {
-            if (contains(polys[i], polys[j])) {
-                poly_adj[i].push_back(j);
-                poly_par[j].push_back(i);
-            }
-        }
-    }
+	ll max_count = 0;
 
-    pts.resize(C);
+	for (auto i: ans) {
+		//for (auto j: i.f) cout << j << " ";
+		//cout << "   " << i.s << "\n";
+		max_count = max(max_count, i.s);
+	}
 
-    for (ll i = 0; i < C; i++) {
-        cin >> pts[i].x >> pts[i].y;
-    }
-
-    ll total = 0;
-    ll ans = 0;
-
-    for (ll i = 0; i < comps; i++) {
-        get_points_in(i);
-        ans = max(ans, points_in[i]);
-        total += points_in[i];
-    }
-
-    ans = max(ans, C - total);
-
-    cout << ans << "\n";
+	cout << max_count << "\n";
 	
 	return 0;
 }
