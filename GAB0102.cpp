39968ce1e9c981ae8e76454834d65bfd2b4450c3
chenant2017 <chenant2017@gmail.com>
2023-02-05 20:54:32 -0800

    AC

--- GAB0102.cpp
+++ GAB0102.cpp
@@ -9,22 +9,20 @@ ll dp[MAX][MAX];
 
 void solve_dp() {
     dp[1][0] = 1;
+
     for (ll i = 2; i < MAX; i++) {
         for (ll j = 0; j < MAX; j++) {
-            if (j > 0) {
-                dp[i][j] += dp[i - 1][j - 1];
-            }
-            dp[i][j] += dp[i - 1][j];
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
         }
     }
 }
 
-ll get_prev(ll a, ll zeros) {
+ll get_prev(ll a) {
     ll result = 0;
 
     for (ll i = 1; i <= a; i++) {
-        for (ll j = i - 1; j >= 0 && j + a - i >= zeros; j--) {
-            cout << i << " " << j << " " << dp[i][j] << "ij\n";
+        for (ll j = i - 1; j >= (i + 1) / 2; j--) {
+            //cout << i << " " << j << " " << dp[i][j] << "ij\n";
             result += dp[i][j];
         }
     }
@@ -32,60 +30,56 @@ ll get_prev(ll a, ll zeros) {
     return result + 1;
 }
 
+ll up_to(ll i, ll j) {
+    ll result = 0;
+    for (ll k = i - 1; k >= j; k--) {
+        result += dp[i][k];
+    }
+    return result;
+}
+
 ll get_round(ll a) {
-    if (a == 1) return 0;
-    //cout << a << " a\n";
+    if (a < 2) return 1;
 
     ll len = floor(log2(a)) + 1;
+    ll result = get_prev(len - 1);
+    ll zeros = 0;
 
-    ll i = len - 2;
+    //cout << result << " res\n";
 
-    while (i >= 0) {
-        if ((a & (1 << i)) != 0) {
-            break;
+    for (ll i = len - 2; i >= 0; i--) {
+        //cout << i << " " << result << "\n";
+        if ((a & (1 << i)) == 0) {
+            zeros++;
         }
-        i--;
+        else {
+            ll need = max(0ll, (len + 1) / 2 - zeros - 1);
+            //cout <<  up_to(i + 1, need) << " " << i + 1 << " " << need << "\n";
+            result += up_to(i + 1, need);
+        }
     }
 
-    //cout << i << " i\n";
+    if (zeros >= (len + 1) / 2) result++;
 
-    if (i == -1) return 1;
-
-    ll zeros = max(0ll, (ll) ceil((double) len / 2.0 ) - (len - 1 - i));
-
-    cout << zeros << "\n";
- 
-    //cout << get_round(a % (1 << (i + 1))) << " " << get_prev(i, zeros) << "result\n";
-
-    return get_round(a % (1 << (i + 1))) 
-    + get_prev(i, zeros);
+    return result;
 }
 
-ll get_sol(ll a) {
-    ll len = floor(log2(a)) + 1;
-    ll z = ceil((double) len / 2.0);
-
-    return get_round(a) + get_prev(len - 1, z);
-}
 
 int main() {
-	//ios_base::sync_with_stdio(false);
-	//cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	
 	string fname = "GAB0102";
 	freopen((fname + ".in").c_str(), "r", stdin);
 	//freopen((fname + ".out").c_str(), "w", stdout);
 	
-	cin >> A >> B;
+    cin >> A >> B;
 
     solve_dp();
 
-    cout << get_sol(8) << " sol\n";
-    cout << get_prev(2, 1) << " p\n";
-
-    return 0;
-
-    cout << get_sol(B) - get_sol(A - 1) << "\n";
+    //cout << get_round(12) << "\n";
 	
+    cout << get_round(B) - get_round(A - 1) << "\n";
+
 	return 0;
 }
