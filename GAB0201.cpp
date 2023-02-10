10a8e99be0564efd12a4526708cf62e39cd4bda9
chenant2017 <chenant2017@gmail.com>
2023-02-09 21:02:28 -0800

    incomplete

---
+++ GAB0201.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define MAXN 40110
+using namespace std;
+
+typedef long long ll;
+
+ll N, O, F, M;
+set<ll> faves;
+map<ll, ll> visits;
+vector<ll> choices;
+ll dp[MAXN];
+
+ll get_dp(ll i) {
+    for (auto c: choices) {
+        dp[i] = max(dp[i], dp[i + c] + c);
+    }
+    return dp[i];
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "GAB0201";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	//freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N >> O >> F >> M;
+
+    for (ll i = 0; i <= N + M; i++) {
+        dp[i] = -1;
+    }
+
+    for (ll i = 0; i < O; i++) {
+        ll c;
+        cin >> c;
+        choices.push_back(c);
+    }
+
+    for (ll i = 0; i < F; i++) {
+        ll f;
+        cin >> f;
+        faves.insert(f);
+    }
+
+    ll i = N;
+    ll visits = 0;
+
+    while (visits <= F * F && i >= 0) {
+        cout << i << "\n";
+
+        get_dp(i);
+        
+        if (faves.find(i) != faves.end()) {
+            visits++;
+
+            cout << dp[i] << " " << dp[i + M] << "\n";
+            if (dp[i] > dp[i + M]) {
+                dp[i + M] = dp[i];
+                i += M;
+                continue;
+            }
+        }
+        i--;
+    }
+
+    if (visits > F * F) {
+        cout << "-1\n";
+    }
+    else {
+        cout << *max_element(dp, dp + N + M + 1) << "\n";
+    }
+	
+	return 0;
+}
