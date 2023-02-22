2269ede4b7c4ca478c0e160118225b207b971e18
chenant2017 <chenant2017@gmail.com>
2023-02-21 16:46:19 -0800

    10/12

---
+++ 190201.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define MAXN 100010
+using namespace std;
+
+typedef long long ll;
+
+ll N;
+vector<ll> cows;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string fname = "herding";
+	freopen((fname + ".in").c_str(), "r", stdin);
+	freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N;
+
+    cows.resize(N);
+
+    for (ll i = 0; i < N; i++) {
+        cin >> cows[i];
+    }
+
+    sort(cows.begin(), cows.end());
+
+    ll ans2 = max(cows[N - 2] - cows[0], cows[N - 1] - cows[1]) + 2 - N;
+    ll ans1 = 1e18;
+
+    for (ll i = 0; i < N; i++) {
+        if (cows[i] + N - 1 > cows[N - 1]) break;
+
+        auto it = upper_bound(cows.begin(), cows.end(), cows[i] + N - 1);
+        it--;
+
+        ll count = distance(cows.begin(), it) - i + 1;
+
+        ans1 = min(ans1, N - count);
+    }
+
+    cout << ans1 << "\n";
+    cout << ans2 << "\n";
+	
+	return 0;
+}
