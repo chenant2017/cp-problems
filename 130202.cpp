3003970113e7d72abe515dae303c6389bbcb2cac
chenant2017 <chenant2017@gmail.com>
2023-02-24 13:18:31 -0800

    AC
    sort by a, then sort by b

--- 130202.cpp
+++ 130202.cpp
@@ -6,75 +6,11 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
+typedef pair<pll, ll> ppll;
 
 ll N;
-pll all[MAX];
-vector<ll> incr;
-vector<ll> decr;
-bool invalid[MAX]; 
-
-void process_same(vector<ll>& v) {
-    ll max_end = -1e18;
-    ll j = -1;
-
-    for (auto i: v) {
-        ll p, q;
-        tie(p, q) = all[i];
-        if (p > q) swap(p, q);
-
-        if (q <= max_end) {
-            invalid[i] = true;
-            if (j != -1) invalid[j] = true;
-        }
-
-        if (q > max_end) {
-            max_end = q;
-            j = i;
-        }
-    }
-}
-
-void process_diff() {
-    stack<ll> st;
-
-    for (ll i = 0; i < N; i++) {
-        if (all[i].f < all[i].s) {
-            st.push(i);
-        }
-        else {
-            while (!st.empty()) {
-                ll j = st.top();
-
-                if (all[i].s < all[j].s) {
-                    invalid[i] = true;
-                    invalid[j] = true;
-                }
-
-                st.pop();
-            }
-        }
-    }
-
-    while (!st.empty()) st.pop();
-
-    for (ll i = 0; i < N; i++) {
-        if (all[i].f > all[i].s) {
-            st.push(i);
-        }
-        else {
-            while (!st.empty()) {
-                ll j = st.top();
-
-                if (all[i].f < all[j].f) {
-                    invalid[i] = true;
-                    invalid[j] = true;
-                }
-
-                st.pop();
-            }
-        }
-    }
-}
+ppll paths[MAX];
+bool invalid[MAX];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -82,42 +18,52 @@ int main() {
 	
 	string fname = "crossings";
 	freopen((fname + ".in").c_str(), "r", stdin);
-	//freopen((fname + ".out").c_str(), "w", stdout);
+	freopen((fname + ".out").c_str(), "w", stdout);
+	
+	cin >> N;
 
-    cin >> N;
-
-    for (ll i = 0; i < N; i++) {
+    for (ll i = 0; i < N; i++) { 
         ll a, b;
         cin >> a >> b;
-
-        all[i] = {a, b};
+        paths[i] = {{a, b}, i};
     }
 
-    sort(all, all + N, [](auto& a, auto& b) { 
-        ll a1 = min(a.f, a.s), a2 = max(a.f, a.s);
-        ll b1 = min(b.f, b.s), b2 = max(b.f, b.s);
-        return a1 < b1 || (a1 == b1 && a2 < b2); 
-    });
+    sort(paths, paths + N);
+
+    ll maxab= -1e18;
 
     for (ll i = 0; i < N; i++) {
-        if (all[i].f <= all[i].s) {
-            incr.push_back(i);
+        if (paths[i].f.s < maxab) {
+            invalid[paths[i].s] = true;
+            //cout << i << "\n";
         }
-        if (all[i].s <= all[i].f) {
-            decr.push_back(i); 
+        else {  
+            maxab = paths[i].f.s;
         }
     }
 
-    process_same(incr); process_same(decr);
-    process_diff();
+    sort(paths, paths + N, [](auto& a, auto& b) {
+        return a.f.s < b.f.s || (a.f.s == b.f.s && a.f.f < b.f.f);
+    });
+
+    maxab = -1e18;
+
+    for (ll i = 0; i < N; i++) {
+        if (paths[i].f.f < maxab) {
+            invalid[paths[i].s] = true;
+            //cout << i << "\n";
+        }
+        else {
+            maxab = paths[i].f.f;
+        }
+    }
 
     ll ans = 0;
-
     for (ll i = 0; i < N; i++) {
         ans += !invalid[i];
     }
 
     cout << ans << endl;
-	
-	return 0;
+
+    return 0;
 }
