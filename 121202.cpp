561c88aebd0be2da9fb51c71cbacd03bc842d9fd
chenant2017 <chenant2017@gmail.com>
2023-02-24 21:19:22 -0800

    AC

--- 121202.cpp
+++ 121202.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
-#define MAX 50010
-#define f first
+#define f first 
 #define s second
+#define MAX 50010
 using namespace std;
 
 typedef long long ll;
-typedef pair<string, ll> psl;
+typedef tuple<string, ll, ll> tp;
 
 ll N;
-string names[MAX];
-set<psl> asc;
-set<psl> desc;
-ll tops[MAX];
-ll bottoms[MAX];
+vector<tp> names;
+ll top[MAX];
+ll bottom[MAX];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -25,53 +23,30 @@ int main() {
 	cin >> N;
 
     for (ll i = 0; i < N; i++) {
-        string s;
-        cin >> s;
-
-        names[i] = s;
-        sort(s.begin(), s.end());
-        asc.insert({s, i});
-        reverse(s.begin(), s.end());
-        desc.insert({s, i});
+        string st;
+        cin >> st;
+        sort(st.begin(), st.end());
+        names.emplace_back(st, 0, i);
+        reverse(st.begin(), st.end());
+        names.emplace_back(st, 1, i);
     }
 
-    for (auto i: asc) {
-        desc.insert({i.f, -1});
-        auto it = desc.find({i.f, -1});
+    sort(names.begin(), names.end());
 
-        reverse(i.f.begin(), i.f.end());
-        auto it2 = desc.find({i.f, i.s});
-        reverse(i.f.begin(), i.f.end());
-
-        ll top = distance(desc.begin(), it);
-        if (distance(desc.begin(), it2) < top) {
-            top--;
+    ll asc = 0, desc = 0;
+    for (auto [a, b, c]: names) {
+        if (b == 0) {
+            asc++;
+            top[c] = desc + 1;
         }
-        tops[i.s] = top;
-
-        desc.erase({i.f, -1});
-    }
-
-    for (auto i: desc) {
-        asc.insert({i.f, -1});
-        auto it = asc.find({i.f, -1});
-
-        reverse(i.f.begin(), i.f.end());
-        auto it2 = asc.find({i.f, i.s});
-        reverse(i.f.begin(), i.f.end());
-
-        ll bottom = distance(asc.begin(), it);
-        if (distance(asc.begin(), it2) < bottom) {
-            bottom--;
+        else {
+            desc++;
+            bottom[c] = asc;
         }
-        bottoms[i.s] = bottom;
-
-        asc.erase({i.f, -1});
     }
-
 
     for (ll i = 0; i < N; i++) {
-        cout << tops[i] + 1 << " " << bottoms[i] + 1 << endl;
+        cout << top[i] << " " << bottom[i] << endl;
     }
 	
 	return 0;
