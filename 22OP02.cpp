7b26db7d959b260a6e19f3e19c28d7e90f8d0af6
chenant2017 <chenant2017@gmail.com>
2023-03-18 21:26:31 -0700

    10/11

--- 22OP02.cpp
+++ 22OP02.cpp
@@ -26,16 +26,6 @@ bool valid(pc p) {
     return u == v;
 }
 
-ll count(char c, string str) {
-    ll result = 0;
-
-    for (auto i: str) {
-        result += i == c;    
-    }
-
-    return result;
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -46,8 +36,8 @@ int main() {
 	
 	cin >> s >> t;
 
-    for (char c1 = 'a'; c1 < 'r'; c1++) {
-        for (char c2 = c1 + 1; c2 <= 'r'; c2++) {
+    for (char c1 = 'a'; c1 <= 'r'; c1++) {
+        for (char c2 = c1; c2 <= 'r'; c2++) {
             if (valid({c1, c2})) {
                 valids.insert({c1, c2});
             }
@@ -63,18 +53,14 @@ int main() {
 
         bool result = true;
 
-        if (q.size() != 1) {
-            for (ll i = 0; i < q.size() - 1; i++) {
-                for (ll j = i + 1; j < q.size(); j++) {
-                    if (valids.find({q[i], q[j]}) == valids.end()) {
-                        result = false;
-                        break;
-                    }
+        for (ll i = 0; i < q.size(); i++) {
+            for (ll j = i; j < q.size(); j++) {
+                //cout << q[i] << " " << q[j] << endl;
+                if (valids.find({q[i], q[j]}) == valids.end()) {
+                    result = false;
+                    break;
                 }
             }
-        }
-        else {
-            result = count(q[0], s) == count(q[0], t);
         }
 
         if (result) {
