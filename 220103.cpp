776c04a02e699112d41b7b8cf31b3ff82aaf60af
chenant2017 <chenant2017@gmail.com>
2023-03-26 13:28:45 -0700

    AC
    vv and vv2 are different and you must check if vv2 is true in the loop, not vv

--- 220103.cpp
+++ 220103.cpp
@@ -1,55 +1,61 @@
 #include <bits/stdc++.h>
 #define MAX 100010
-#define f first 
-#define s second
 using namespace std;
 
 typedef long long ll;
-typedef pair<ll, ll> pll;
 
-ll N, M;
-pll edges[MAX];
-vector<pll> adj[MAX];
-bool visited[MAX];
-bool dfs_visited[MAX];
-bool e_visited[MAX];
-bool fed[MAX];
+struct Edge {
+    ll to, id;
+    bool start; // is it the starting end
+
+    Edge(ll a, ll b, bool c) {
+        to = a;
+        id = b;
+        start = c;
+    }
+};
+
+ll N, M, R;
+ll v, e; // chosen at beginning
+vector<Edge> adj[MAX];
+bool ev[MAX];
+bool vv[MAX];
+
+bool ev2[MAX];
+bool vv2[MAX];
 vector<ll> ans;
 
-ll edge_ignore;
-ll start;
-
-void get_start(ll i) {
-    if (visited[i]) return;
-    visited[i] = true;
+void get_start(ll i) { //
+    vv[i] = true;
 
     for (auto j: adj[i]) {
-        if (e_visited[j.s]) continue;
-        e_visited[j.s] = true;
-        if (visited[j.f]) {
-            
-            edge_ignore = j.s;
-            start = edges[j.s].f;
+        if (ev[j.id]) continue;
+        ev[j.id] = true;
+        if (vv[j.to]) {
+            e = j.id;
+            if (j.start) {
+                v = i;
+            }
+            else {
+                v = j.to;
+            }
             return;
         }
-        get_start(j.f);
+
+        get_start(j.to);
     }
 }
 
+
 void dfs(ll i) {
-    //cout << i << "i\n";
-    if (dfs_visited[i]) return;
-    dfs_visited[i] = true;
+    vv2[i] = true;
 
     for (auto j: adj[i]) {
-        if (j.s == edge_ignore) {
-            continue;
-        }
-        if (!dfs_visited[j.f]) {
-            fed[j.s] = true;
-            ans.push_back(j.s);
-            dfs(j.f);
-        }
+        if (ev2[j.id]) continue;
+        if (vv2[j.to]) continue;
+        ev2[j.id] = true;
+        ans.push_back(j.id);
+        dfs(j.to);
     }
 }
 
@@ -64,39 +70,32 @@ int main() {
 	cin >> N >> M;
 
     for (ll i = 1; i <= N; i++) {
-        ll a, b;
-        cin >> a >> b;
-        edges[i] = {a, b};
-        adj[a].push_back({b, i});
-        adj[b].push_back({a, i});
+        ll f, s;
+        cin >> f >> s;
+        adj[f].push_back(Edge(s, i, true));
+        adj[s].push_back(Edge(f, i, false));
     }
 
     for (ll i = 1; i <= M; i++) {
-        edge_ignore = -1;
-        start = -1;
-
-        if (dfs_visited[i]) continue;
-
+        if (vv2[i]) continue;
+        v = i;
+        e = -1;
         get_start(i);
 
-        if (start == -1) {
-            start = i;
+        //cout << v << " " << e << "\n";
+
+        if (e != -1) {
+            ev2[e] = true;
+            ans.push_back(e);
         }
-
-        //cout << start << " " << edge_ignore << "\n";
-
-        if (edge_ignore != -1) {
-            fed[edge_ignore] = true;
-            ans.push_back(edge_ignore);
-        }
-
-        dfs(start);
+        
+        dfs(v);
     }
 
     ll hungry = 0;
 
     for (ll i = 1; i <= N; i++) {
-        if (!fed[i]) {
+        if (!ev2[i]) {
             ans.push_back(i);
             hungry++;
         }
@@ -108,6 +107,5 @@ int main() {
         cout << i << "\n";
     }
 
-	
 	return 0;
 }
